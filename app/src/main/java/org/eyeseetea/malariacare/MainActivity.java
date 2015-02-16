aaaaef4be12e55b042d010ee61d6f024e7f52507
nachofoche <ifoche@cnb.csic.es>
2015-02-16 03:07:03 +0100

    Merge to nacho_layout done. Good behavior, when commenting Adrian onCreate stuff. Now going for checking Adrian system is working

--- app/src/main/java/org/eyeseetea/malariacare/MainActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/MainActivity.java
@@ -10,11 +10,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import java.util.ArrayList;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 
 import org.eyeseetea.malariacare.data.Header;
-import org.eyeseetea.malariacare.data.Question;
-import org.eyeseetea.malariacare.data.Tab;
+//import org.eyeseetea.malariacare.data.Question;
+//import org.eyeseetea.malariacare.data.Tab;
 import org.eyeseetea.malariacare.utils.PopulateDB;
+import java.util.Random;
+import static junit.framework.Assert.*;
+
 
 import java.util.List;
 //import org.eyeseetea.malariacare.database.MalariaCareDbHelper;
@@ -22,13 +32,158 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
+    protected List<Question> getQuestionSet(int size) {
+        // Just to be able to work, I need a question simulator, in order to emulate the DB entries
+        Random r = new Random();
+        String optionSets[] = {
+                "asked",
+                "done",
+                "yesno",
+                "yesNoNA",
+                "yesNoAsked",
+                "yesNoUnkasked",
+                "gender",
+                "officer",
+                "malResults",
+                "malDiagnose",
+                "malSpecies",
+                "result"
+        };
+        Log.i(".MainActivity", "optionSet created");
+        List<Question> questionSimulator = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Log.i(".MainActivity", "creating question " + i);
+            // The text of the question will be fixed
+            String statement = "Question number " + Integer.toString(i);
+            // Te OptionSet will be randomized
+            String optionSet = optionSets[r.nextInt(optionSets.length)];
+            Log.i(".MainActivity", "optionSet " + optionSet);
+            // We finally add both to the question array
+            questionSimulator.add(new Question(statement, optionSet));
+            Log.i(".MainActivity", "question finished");
+        }
+        Log.i(".MainActivity", "questions simulated");
+        return questionSimulator;
+    }
+
+    protected List<Tab> getTabSet(int size){
+        // Just to be able to work, I need a question simulator, in order to emulate the DB entries
+        Random r = new Random();
+        String tabTypes [] = {
+                "Score",
+                "GNR1",
+                "Microscopy1",
+                "RDT1",
+                "GNR2",
+                "Microscopy2",
+                "RDT2",
+                "GNR3",
+                "Microscopy3",
+                "GNR3"
+        };
+        Log.i(".MainActivity","tabSet created");
+        List<Tab> tabSimulator = new ArrayList<>();
+        for (int i=0; i<size; i++) {
+            Log.i(".MainActivity", "creating tab " + i);
+            // Te OptionSet will be randomized
+            String tabType = tabTypes[r.nextInt(tabTypes.length)];
+            Log.i(".MainActivity", "tabType " + tabType);
+            // We finally add both to the question array
+            tabSimulator.add(new Tab(tabType));
+            Log.i(".MainActivity", "tab finished");
+        }
+        Log.i(".MainActivity","tabs simulated");
+        return tabSimulator;
+    }
+
+    protected int insertLayout(Question question, GridLayout parent){
+        String layout = question.getOptionSet();
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v;
+        TextView statement;
+        int child = -1;
+        switch(layout) {
+            case "asked":
+                child = R.layout.asked;
+                break;
+            case "done":
+                child = R.layout.done;
+                break;
+            case "yesno":
+                child = R.layout.yesno;
+                break;
+            case "yesNoNA":
+                child = R.layout.yesnona;
+                break;
+            case "yesNoAsked":
+                child = R.layout.yesnonotanswered;
+                break;
+            case "yesNoUnkasked":
+                child = R.layout.yesnounkasked;
+                break;
+            case "gender":
+                child = R.layout.gender;
+                break;
+            case "officer":
+                child = R.layout.officer;
+                break;
+            case "malResults":
+                child = R.layout.malresults;
+                break;
+            case "malDiagnose":
+                child = R.layout.maldiagnose;
+                break;
+            case "malSpecies":
+                child = R.layout.malspecies;
+                break;
+            case "result":
+                child = R.layout.result;
+                break;
+         }
+        Log.i(".MainActivity", "question statement: " + question.getStatement());
+        v = inflater.inflate(child, parent, false);
+        statement = (TextView) v.findViewById(R.id.statement);
+        Log.i(".MainActivity", "previous statement: " + statement.getText());
+        statement.setText(question.getStatement());
+        Log.i(".MainActivity", "later statement: " + statement.getText());
+        parent.addView(v);
+        // For not found layout, child will be -1
+        return child;
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(".MainActivity", "App started");
+        setContentView(R.layout.main_layout);
+
+        // We get a set of questions for our layout
+        List<Question> questions;
+        questions = getQuestionSet(100);
+
+        // We get a set of tabs
+        List<Tab> tabs;
+        tabs = getTabSet(3);
+
+        // We take the Layouts for adding the content
+        GridLayout body = (GridLayout) this.findViewById(R.id.Body);
+
+        // We add the questions
+        for (Question question : questions) {
+            /* Each optionSet has its own layout defined by <optionSetString>.xml
+               With the insertLayout function, we're trying to insert the question layout into the
+                parent layout. The function returns the question layout. We assert is always != -1
+             */
+            assertTrue(insertLayout(question, body) != -1);
+        }
+
 
 //        File dbFile = getDatabasePath("malariacare.db");
 //        adb pull /data/data/org.eyeseetea.malariacare/databases/malariacare.db ~/malariacare.db
-
+/*
         if (Tab.count(Tab.class, null, null)==0) {
             AssetManager assetManager = getAssets();
             PopulateDB.populateDB(assetManager);
@@ -79,7 +234,7 @@ public class MainActivity extends ActionBarActivity {
         et.setLayoutParams(layoutParams);
         linearLayout.addView(et);
 
-        setContentView(linearLayout, layoutParams);
+        setContentView(linearLayout, layoutParams);*/
     }
 
 
@@ -107,6 +262,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void sendMessage(View view) {
-        System.out.println("taka");
+        Log.i(".MainActivity", "Button pressed");
     }
 }
