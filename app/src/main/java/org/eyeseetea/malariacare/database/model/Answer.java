1ee5e02d6edf36b9a052797ea246519014f0d9f7
ifoche <nacho.foche@gmail.com>
2015-03-17 17:07:10 +0100

    Copyright added to project as GPLv3. Now we need to be able to show the license text

--- app/src/main/java/org/eyeseetea/malariacare/database/model/Answer.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/model/Answer.java
@@ -1,12 +1,28 @@
+/*
+ * Copyright (c) 2015.
+ *
+ * This file is part of Facility QA Tool App.
+ *
+ *  Facility QA Tool App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Facility QA Tool App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.eyeseetea.malariacare.database.model;
 
 import com.orm.SugarRecord;
 
 import java.util.List;
 
-/**
- * Created by adrian on 14/02/15.
- */
 public class Answer extends SugarRecord<Answer> {
 
     String name;
