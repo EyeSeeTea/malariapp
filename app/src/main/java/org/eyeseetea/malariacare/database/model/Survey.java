5fd1fff4c322cfd61aad24f24345243d5a070e06
ifoche <nacho.foche@gmail.com>
2015-04-15 02:27:41 +0200

    oops, I left this classes behind

---
+++ app/src/main/java/org/eyeseetea/malariacare/database/model/Survey.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2015.
+ *
+ * This file is part of Facility QA Tool App.
+ *
+ *  Facility QA Tool App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Facility QA Tool App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eyeseetea.malariacare.database.model;
+
+import com.orm.SugarRecord;
+
+import java.util.Date;
+
+public class Survey extends SugarRecord<Survey> {
+
+    OrgUnit orgUnit;
+    Program program;
+    User user;
+    String eventDate;
+
+    public Survey() {
+    }
+
+    public Survey(OrgUnit orgUnit, Program program, User user) {
+        this.orgUnit = orgUnit;
+        this.program = program;
+        this.user = user;
+        this.eventDate = new Date().toString();
+    }
+
+    public Survey(OrgUnit orgUnit, Program program, User user, String eventDate) {
+        this.orgUnit = orgUnit;
+        this.program = program;
+        this.user = user;
+        this.eventDate = eventDate;
+    }
+
+    public OrgUnit getOrgUnit() {
+        return orgUnit;
+    }
+
+    public void setOrgUnit(OrgUnit orgUnit) {
+        this.orgUnit = orgUnit;
+    }
+
+    public Program getProgram() {
+        return program;
+    }
+
+    public void setProgram(Program program) {
+        this.program = program;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Survey)) return false;
+
+        Survey survey = (Survey) o;
+
+        if (!eventDate.equals(survey.eventDate)) return false;
+        if (!orgUnit.equals(survey.orgUnit)) return false;
+        if (!program.equals(survey.program)) return false;
+        if (!user.equals(survey.user)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = orgUnit.hashCode();
+        result = 31 * result + program.hashCode();
+        result = 31 * result + user.hashCode();
+        result = 31 * result + eventDate.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Survey{" +
+                "orgUnit=" + orgUnit +
+                ", program=" + program +
+                ", user=" + user +
+                ", eventDate='" + eventDate + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/org/eyeseetea/malariacare/database/model/User.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015.
+ *
+ * This file is part of Facility QA Tool App.
+ *
+ *  Facility QA Tool App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Facility QA Tool App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eyeseetea.malariacare.database.model;
+
+import com.orm.SugarRecord;
+
+public class User extends SugarRecord<User> {
+
+    String uid;
+    String name;
+
+    public User() {
+    }
+
+    public User(String uid, String name) {
+        this.uid = uid;
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (!uid.equals(user.uid)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uid.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
