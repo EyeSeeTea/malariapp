674f264216564e2b67fe7d4b7a3a1b3f93b9bba7
ifoche <nacho.foche@gmail.com>
2015-04-30 02:10:28 +0200

    menu little bug re-solved

--- app/src/main/java/org/eyeseetea/malariacare/SurveyActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/SurveyActivity.java
@@ -83,7 +83,7 @@ public class SurveyActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.layout.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
