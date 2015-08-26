2e8e52d8201917ba4260ced5e9da52dd0724ff68
ifoche <nacho.foche@gmail.com>
2015-08-26 23:40:02 +0200

    hidding keyboard when scroll

--- app/src/main/java/org/eyeseetea/malariacare/SurveyActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/SurveyActivity.java
@@ -32,6 +32,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
@@ -515,6 +516,8 @@ public class SurveyActivity extends BaseActivity{
                 View currentFocus = getCurrentFocus();
                 if (currentFocus != null) {
                     currentFocus.clearFocus();
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
             }
         }
