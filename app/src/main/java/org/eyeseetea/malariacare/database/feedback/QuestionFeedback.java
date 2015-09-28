acaa12266deb84a3cf289921409e4f09f5393aca
ifoche <nacho.foche@gmail.com>
2015-09-28 18:25:22 +0200

    feedback shown also for pass questions

--- app/src/main/java/org/eyeseetea/malariacare/database/feedback/QuestionFeedback.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/feedback/QuestionFeedback.java
@@ -94,12 +94,6 @@ public class QuestionFeedback implements Feedback {
      * @return
      */
     public String getFeedback(){
-
-        //Pass->null (cannot return 'No feedback' due to i18N
-        if(this.isPassed()){
-            return null;
-        }
-
         String questionFeedback=this.question.getFeedback();
         //XXX Temporal hack to show some demo feedback
         if(questionFeedback!=null && !questionFeedback.isEmpty()){
