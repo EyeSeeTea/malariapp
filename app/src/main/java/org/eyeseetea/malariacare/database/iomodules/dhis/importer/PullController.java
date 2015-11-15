ac8cf003956411a98b038aa59fd9943d0c10d99d
idelcano <ignaciodelcano@gmail.com>
2015-11-15 10:33:40 +0100

    Added Question and question relations call

--- app/src/main/java/org/eyeseetea/malariacare/database/iomodules/dhis/importer/PullController.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/iomodules/dhis/importer/PullController.java
@@ -58,7 +58,6 @@ import org.hisp.dhis.android.sdk.persistence.Dhis2Application;
 import org.hisp.dhis.android.sdk.persistence.models.DataElement;
 import org.hisp.dhis.android.sdk.persistence.models.OptionSet;
 import org.hisp.dhis.android.sdk.persistence.models.OrganisationUnit;
-import org.hisp.dhis.android.sdk.persistence.models.UserAccount;
 import org.hisp.dhis.android.sdk.persistence.preferences.ResourceType;
 import java.util.List;
 
@@ -223,6 +222,12 @@ public class PullController {
             DataElementExtended dataElementExtended = new DataElementExtended(dataElement);
             dataElementExtended.accept(converter);
         }
+
+        //Convert questions and compositeScores
+        for(DataElement dataElement:dataElementList){
+            converter.buildRelations(dataElement);
+        }
+
         //Fill order and parent scores
         converter.buildScores();
     }
