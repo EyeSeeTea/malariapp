227ee6d34e4a96cd4156199d1d61dc42fb1e44db
Ivan Arrizabalaga <ivan.arrizabalaga@gmail.com>
2015-12-04 19:28:05 +0100

    Recreates tables after update

--- app/src/main/java/org/eyeseetea/malariacare/database/migrations/Migration1RestartDB.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/migrations/Migration1RestartDB.java
@@ -29,6 +29,23 @@ import com.raizlabs.android.dbflow.structure.ModelAdapter;
 
 import org.eyeseetea.malariacare.BaseActivity;
 import org.eyeseetea.malariacare.database.AppDatabase;
+import org.eyeseetea.malariacare.database.model.Answer;
+import org.eyeseetea.malariacare.database.model.CompositeScore;
+import org.eyeseetea.malariacare.database.model.Header;
+import org.eyeseetea.malariacare.database.model.Match;
+import org.eyeseetea.malariacare.database.model.Option;
+import org.eyeseetea.malariacare.database.model.OrgUnit;
+import org.eyeseetea.malariacare.database.model.OrgUnitLevel;
+import org.eyeseetea.malariacare.database.model.Program;
+import org.eyeseetea.malariacare.database.model.Question;
+import org.eyeseetea.malariacare.database.model.QuestionOption;
+import org.eyeseetea.malariacare.database.model.QuestionRelation;
+import org.eyeseetea.malariacare.database.model.Score;
+import org.eyeseetea.malariacare.database.model.Survey;
+import org.eyeseetea.malariacare.database.model.Tab;
+import org.eyeseetea.malariacare.database.model.TabGroup;
+import org.eyeseetea.malariacare.database.model.User;
+import org.eyeseetea.malariacare.database.model.Value;
 import org.eyeseetea.malariacare.database.utils.PopulateDB;
 import org.eyeseetea.malariacare.database.utils.PreferencesState;
 import org.hisp.dhis.android.sdk.persistence.models.Attribute;
@@ -42,31 +59,58 @@ import java.io.IOException;
 @Migration(version = 2, databaseName = AppDatabase.NAME)
 public class Migration1RestartDB extends BaseMigration {
 
+    private final static String TAG=".Migration";
+
+    private final static Class APP_TABLES_TO_UPDATE[] = {
+            Value.class,
+            Score.class,
+            Survey.class,
+            OrgUnit.class,
+            OrgUnitLevel.class,
+            User.class,
+            QuestionOption.class,
+            Match.class,
+            QuestionRelation.class,
+            Question.class,
+            CompositeScore.class,
+            Option.class,
+            Answer.class,
+            Header.class,
+            Tab.class,
+            TabGroup.class,
+            Program.class
+    };
+
+    private final static Class SDK_TABLES_TO_UPDATE[] = {
+            Attribute.class,
+            DataElement.class
+    };
+    public static final String DROP_TABLE_IF_EXISTS = "DROP TABLE IF EXISTS ";
+
     public Migration1RestartDB() {
         super();
     }
 
     public void onPreMigrate() {
-        FlowManager.getDatabase(AppDatabase.NAME).reset(PreferencesState.getInstance().getContext());
+//        FlowManager.getDatabase(AppDatabase.NAME).reset(PreferencesState.getInstance().getContext());
     }
 
     @Override
     public void migrate(SQLiteDatabase database) {
-        try {
-            ModelAdapter myAdapter = FlowManager.getModelAdapter(Attribute.class);
-            database.execSQL("DROP TABLE IF EXISTS " + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-            myAdapter = FlowManager.getModelAdapter(DataElement.class);
-            database.execSQL("DROP TABLE IF EXISTS " + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-        } catch (Exception e) {
-            Log.i(".migrate", "Error");
-            e.printStackTrace();
-        }
+        recreateTables(database,APP_TABLES_TO_UPDATE);
+        recreateTables(database,SDK_TABLES_TO_UPDATE);
     }
 
     @Override
     public void onPostMigrate() {
         //release migration resources
+    }
+
+    private void recreateTables(SQLiteDatabase database,Class[] tables){
+        for(int i=0;i<tables.length;i++){
+            ModelAdapter myAdapter = FlowManager.getModelAdapter(tables[i]);
+            database.execSQL(DROP_TABLE_IF_EXISTS + myAdapter.getTableName());
+            database.execSQL(myAdapter.getCreationQuery());
+        }
     }
 }
