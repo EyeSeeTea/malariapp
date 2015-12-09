89605d06991f038c68101d28c66c673b9741a11d
idelcano <ignaciodelcano@gmail.com>
2015-12-09 15:59:48 +0100

    Removed unnused intents in manifest, add migration, and remove incorrect copyright text

--- app/src/main/java/org/eyeseetea/malariacare/database/migration/Migration1PictureaApp.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/migration/Migration1PictureaApp.java
@@ -17,7 +17,7 @@
  *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.eyeseetea.malariacare.database.migration;
+package org.eyeseetea.malariacare.database.migrations;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
@@ -27,54 +27,90 @@ import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.migration.BaseMigration;
 import com.raizlabs.android.dbflow.structure.ModelAdapter;
 
+import org.eyeseetea.malariacare.BaseActivity;
 import org.eyeseetea.malariacare.database.AppDatabase;
-import org.eyeseetea.malariacare.database.model.OptionAttribute;
+import org.eyeseetea.malariacare.database.model.Answer;
+import org.eyeseetea.malariacare.database.model.CompositeScore;
+import org.eyeseetea.malariacare.database.model.Header;
+import org.eyeseetea.malariacare.database.model.Match;
+import org.eyeseetea.malariacare.database.model.Option;
+import org.eyeseetea.malariacare.database.model.OrgUnit;
+import org.eyeseetea.malariacare.database.model.OrgUnitLevel;
+import org.eyeseetea.malariacare.database.model.Program;
+import org.eyeseetea.malariacare.database.model.Question;
+import org.eyeseetea.malariacare.database.model.QuestionOption;
+import org.eyeseetea.malariacare.database.model.QuestionRelation;
+import org.eyeseetea.malariacare.database.model.Score;
 import org.eyeseetea.malariacare.database.model.Survey;
 import org.eyeseetea.malariacare.database.model.Tab;
+import org.eyeseetea.malariacare.database.model.TabGroup;
+import org.eyeseetea.malariacare.database.model.User;
 import org.eyeseetea.malariacare.database.model.Value;
+import org.eyeseetea.malariacare.database.utils.PopulateDB;
 import org.eyeseetea.malariacare.database.utils.PreferencesState;
+import org.hisp.dhis.android.sdk.persistence.models.Attribute;
+import org.hisp.dhis.android.sdk.persistence.models.DataElement;
 
+import java.io.IOException;
 
 /**
  * Created by ignac on 30/11/2015.
  */
 @Migration(version = 2, databaseName = AppDatabase.NAME)
-public class Migration1PictureaApp extends BaseMigration {
+public class Migration1RestartDB extends BaseMigration {
 
-    public Migration1PictureaApp() {
+    private final static String TAG=".Migration";
+
+    private final static Class APP_TABLES_TO_UPDATE[] = {
+            Value.class,
+            Score.class,
+            Survey.class,
+            OrgUnit.class,
+            OrgUnitLevel.class,
+            User.class,
+            QuestionOption.class,
+            Match.class,
+            QuestionRelation.class,
+            Question.class,
+            CompositeScore.class,
+            Option.class,
+            Answer.class,
+            Header.class,
+            Tab.class,
+            TabGroup.class,
+            Program.class
+    };
+
+    private final static Class SDK_TABLES_TO_UPDATE[] = {
+            Attribute.class,
+            DataElement.class
+    };
+    public static final String DROP_TABLE_IF_EXISTS = "DROP TABLE IF EXISTS ";
+
+    public Migration1RestartDB() {
         super();
     }
 
     public void onPreMigrate() {
-        FlowManager.getDatabase(AppDatabase.NAME).reset(PreferencesState.getInstance().getContext());
+//        FlowManager.getDatabase(AppDatabase.NAME).reset(PreferencesState.getInstance().getContext());
     }
 
     @Override
     public void migrate(SQLiteDatabase database) {
-        try {
-            ModelAdapter myAdapter = FlowManager.getModelAdapter(OptionAttribute.class);
-            database.execSQL("DROP TABLE IF EXISTS " + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-
-            myAdapter = FlowManager.getModelAdapter(Survey.class);
-            database.execSQL("DROP TABLE IF EXISTS " + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-            ;
-            myAdapter = FlowManager.getModelAdapter(Tab.class);
-            database.execSQL("DROP TABLE IF EXISTS " + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-
-            myAdapter = FlowManager.getModelAdapter(Value.class);
-            database.execSQL("DROP TABLE IF EXISTS " + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-        } catch (Exception e) {
-            Log.i(".migrate", "Error");
-            e.printStackTrace();
-        }
+        recreateTables(database,APP_TABLES_TO_UPDATE);
+        recreateTables(database,SDK_TABLES_TO_UPDATE);
     }
 
     @Override
     public void onPostMigrate() {
         //release migration resources
+    }
+
+    private void recreateTables(SQLiteDatabase database,Class[] tables){
+        for(int i=0;i<tables.length;i++){
+            ModelAdapter myAdapter = FlowManager.getModelAdapter(tables[i]);
+            database.execSQL(DROP_TABLE_IF_EXISTS + myAdapter.getTableName());
+            database.execSQL(myAdapter.getCreationQuery());
+        }
     }
 }
