ba5f877074b15c8c9fd5cc4aa76d4f14c34c65f3
idelcano <ignaciodelcano@gmail.com>
2015-12-19 05:23:52 +0100

    Modify comments

--- app/src/main/java/org/eyeseetea/malariacare/receivers/AlarmPushReceiver.java
+++ app/src/main/java/org/eyeseetea/malariacare/receivers/AlarmPushReceiver.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2015.
  *
- * This file is part of QIS Survelliance App.
+ * This file is part of Facility QA Tool App.
  *
- *  QIS Survelliance App is free software: you can redistribute it and/or modify
+ *  Facility QA Tool App is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  QIS Survelliance App is distributed in the hope that it will be useful,
+ *  Facility QA Tool App is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with QIS Survelliance App.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Facility QA Tool App.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
