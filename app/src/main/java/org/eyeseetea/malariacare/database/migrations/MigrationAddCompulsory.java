add79ff22e4136d1ce6a15094c5854821d8c878e
idelcano <ignaciodelcano@gmail.com>
2016-01-05 15:26:43 +0100

    Added the migration number 4, added the column compulsory in the Question

--- app/src/main/java/org/eyeseetea/malariacare/database/migrations/MigrationAddCompulsory.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/migrations/MigrationAddCompulsory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015.
+ * Copyright (c) 2016.
  *
  * This file is part of QA App.
  *
@@ -20,14 +20,12 @@
 package org.eyeseetea.malariacare.database.migrations;
 
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import com.raizlabs.android.dbflow.annotation.Migration;
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.migration.BaseMigration;
 import com.raizlabs.android.dbflow.structure.ModelAdapter;
 
-import org.eyeseetea.malariacare.BaseActivity;
 import org.eyeseetea.malariacare.database.AppDatabase;
 import org.eyeseetea.malariacare.database.model.Answer;
 import org.eyeseetea.malariacare.database.model.CompositeScore;
@@ -46,51 +44,20 @@ import org.eyeseetea.malariacare.database.model.Tab;
 import org.eyeseetea.malariacare.database.model.TabGroup;
 import org.eyeseetea.malariacare.database.model.User;
 import org.eyeseetea.malariacare.database.model.Value;
-import org.eyeseetea.malariacare.database.utils.PopulateDB;
-import org.eyeseetea.malariacare.database.utils.PreferencesState;
 import org.hisp.dhis.android.sdk.persistence.models.Attribute;
 import org.hisp.dhis.android.sdk.persistence.models.DataElement;
-
-import java.io.IOException;
 
 /**
  * Created by ignac on 30/11/2015.
  */
-@Migration(version = 3, databaseName = AppDatabase.NAME)
-public class Migration1RestartDB extends BaseMigration {
+@Migration(version = 4, databaseName = AppDatabase.NAME)
+public class MigrationAddCompulsory extends BaseMigration {
 
     private final static String TAG=".Migration";
 
-    private final static Class APP_TABLES_TO_UPDATE[] = {
-            Value.class,
-            Score.class,
-            Survey.class,
-            OrgUnit.class,
-            OrgUnitLevel.class,
-            User.class,
-            QuestionOption.class,
-            Match.class,
-            QuestionRelation.class,
-            Question.class,
-            CompositeScore.class,
-            Option.class,
-            Answer.class,
-            Header.class,
-            Tab.class,
-            TabGroup.class,
-            Program.class
-    };
-
-    private final static Class SDK_TABLES_TO_UPDATE[] = {
-            Attribute.class,
-            DataElement.class
-    };
-
-    public static final String DROP_TABLE_IF_EXISTS = "DROP TABLE IF EXISTS ";
-
     public static final String ALTER_TABLE_ADD_COLUMN = "ALTER TABLE %s ADD COLUMN %s %s";
 
-    public Migration1RestartDB() {
+    public MigrationAddCompulsory() {
         super();
     }
 
@@ -100,12 +67,7 @@ public class Migration1RestartDB extends BaseMigration {
 
     @Override
     public void migrate(SQLiteDatabase database) {
-        try{
-            addColumn(database, Survey.class, "creationDate", "integer");
-            addColumn(database, Survey.class, "scheduledDate", "integer");
-            addColumn(database, Question.class, "output", "integer");
-        }catch(Exception e){}
-        recreateTables(database,SDK_TABLES_TO_UPDATE);
+            addColumn(database, Question.class, "compulsory", "boolean");
     }
 
     @Override
@@ -118,11 +80,4 @@ public class Migration1RestartDB extends BaseMigration {
         database.execSQL(String.format(ALTER_TABLE_ADD_COLUMN, myAdapter.getTableName(),columnName,type) );
     }
 
-    private void recreateTables(SQLiteDatabase database,Class[] tables){
-        for(int i=0;i<tables.length;i++){
-            ModelAdapter myAdapter = FlowManager.getModelAdapter(tables[i]);
-            database.execSQL(DROP_TABLE_IF_EXISTS + myAdapter.getTableName());
-            database.execSQL(myAdapter.getCreationQuery());
-        }
-    }
 }
