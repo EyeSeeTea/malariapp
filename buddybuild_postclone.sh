5627a15bf45ee1243ab5bdb0adb35752776a9054
ifoche <nacho.foche@gmail.com>
2016-02-01 02:58:31 +0100

    cosmetics

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-git checkout development
-rm -rf sdk
-git clone -b 2.22_EyeSeeTea git@github.com:EyeSeeTea/dhis2-android-sdk.git sdk
 rm -rf DBFlowORM
 git clone git@github.com:EyeSeeTea/DBFlow.git DBFlowORM
+rm -rf sdk
+git clone -b 2.22_EyeSeeTea git@github.com:EyeSeeTea/dhis2-android-sdk.git sdk
