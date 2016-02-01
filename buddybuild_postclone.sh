ebf4bad50f1a05a79ec63d2618725ef2ea567f43
ifoche <nacho.foche@gmail.com>
2016-02-01 01:40:16 +0100

    post clone script to make buddybuild work

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+rm -rf sdk
 git clone -b 2.22_EyeSeeTea git@github.com:EyeSeeTea/dhis2-android-sdk.git sdk
+rm -rf DBFlowORM
 git clone git@github.com:EyeSeeTea/DBFlow.git DBFlowORM
