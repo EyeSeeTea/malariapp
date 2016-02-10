03526ba9fe43099e339c72585b6ad5fd20f44687
idelcano <ignaciodelcano@gmail.com>
2016-02-10 21:10:43 +0100

    Changed testing user and added Program attribute(only in sdk)

--- app/src/androidTest/java/org/eyeseetea/malariacare/test/pull/PullOKTest.java
+++ app/src/androidTest/java/org/eyeseetea/malariacare/test/pull/PullOKTest.java
@@ -4,13 +4,11 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.eyeseetea.malariacare.LoginActivity;
-import org.eyeseetea.malariacare.R;
 import org.eyeseetea.malariacare.database.model.OrgUnit;
-import org.eyeseetea.malariacare.database.model.OrgUnitLevel;
 import org.eyeseetea.malariacare.database.model.Program;
-import org.eyeseetea.malariacare.database.model.Survey;
 import org.eyeseetea.malariacare.database.utils.PopulateDB;
 import org.eyeseetea.malariacare.test.utils.SDKTestUtils;
+import org.hisp.dhis.android.sdk.persistence.models.ProgramAttributeValue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -34,6 +32,10 @@ import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPull;
 @RunWith(AndroidJUnit4.class)
 public class PullOKTest {
 
+    private final String ATTRIBUTE_SUPERVISION_CODE="PSupervisor";
+    private final String ATTRIBUTE_SUPERVISION_VALUE="Adrian Quintana";
+    private final String ATTRIBUTE_SUPERVISION_ID="zG5T2x5Yjrx";
+
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
             LoginActivity.class);
@@ -50,11 +52,11 @@ public class PullOKTest {
 
     @Test
     public void pullWithTestUser(){
-        login(HNQIS_DEV_STAGING, TEST_USERNAME_NO_PERMISSION, TEST_PASSWORD_NO_PERMISSION);
+        login(HNQIS_DEV_STAGING, TEST_USERNAME_WITH_PERMISSION, TEST_PASSWORD_WITH_PERMISSION);
 
         waitForPull(20);
 
-        //Create the Test Program and OrgUnit with real data.
+        //Create the Test Program and OrgUnit to compare with real data.
         Program testProgram= new Program("wK0958s1bdj","KE HNQIS Family Planning");
         OrgUnit testOrgUnit= new OrgUnit("QS7sK8XzdQc","KE - HNQIS SF pilot test facility 1",null,null);
         testOrgUnit.setProductivity(10);
@@ -70,10 +72,20 @@ public class PullOKTest {
         //assertTrue(testOrgUnit.getOrgUnitLevel().getName().equals(pulledOrgUnit.getOrgUnitLevel().getName()));
         assertTrue(testProgram.getUid().equals(pulledOrgUnit.getPrograms().get(0).getUid()));
 
-        Program pulledProgram=SDKTestUtils.getProgram(testProgram.getName());
+        org.hisp.dhis.android.sdk.persistence.models.Program pulledProgram=SDKTestUtils.getProgram(testProgram.getName());
 
         assertTrue(testProgram.getUid().equals(pulledProgram.getUid()));
         assertTrue(testProgram.getName().equals(pulledProgram.getName()));
+        List<ProgramAttributeValue> attributeValues=pulledProgram.getAttributeValues();
+        boolean isProductivityCode=false;
+        for(ProgramAttributeValue programAttributeValue:attributeValues) {
+            if (programAttributeValue.getAttribute().getCode().equals(ATTRIBUTE_SUPERVISION_CODE) && programAttributeValue.getAttribute().getUid().equals(ATTRIBUTE_SUPERVISION_ID)) {
+                if(programAttributeValue.getValue().equals(ATTRIBUTE_SUPERVISION_VALUE))
+                    isProductivityCode=true;
+                //Fixme here we need check if the attribute of the program is translate to our app db. But at this moment is not converted from the sdk.
+            }
+        }
+        assertTrue(isProductivityCode);
 
     }
 
