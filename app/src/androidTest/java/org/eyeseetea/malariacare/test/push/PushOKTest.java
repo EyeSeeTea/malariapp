0ea8bcbae9a5163c97cc156baa72d1ffd7d1954b
ifoche <nacho.foche@gmail.com>
2016-02-19 19:12:19 +0100

    revert da4d42e7820c47317bbc9dce8f52d0433b9c0f71

---
+++ app/src/androidTest/java/org/eyeseetea/malariacare/test/push/PushOKTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2016.
+ *
+ * This file is part of QA App.
+ *
+ *  Health Network QIS App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Health Network QIS App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eyeseetea.malariacare.test.push;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.eyeseetea.malariacare.LoginActivity;
+import org.eyeseetea.malariacare.R;
+import org.eyeseetea.malariacare.database.model.Survey;
+import org.eyeseetea.malariacare.database.utils.PopulateDB;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertTrue;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.HNQIS_DEV_STAGING;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_PASSWORD_NO_PERMISSION;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_PASSWORD_WITH_PERMISSION;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_USERNAME_NO_PERMISSION;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_USERNAME_WITH_PERMISSION;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.fillSurvey;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.login;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.markInProgressAsCompleted;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.startSurvey;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPull;
+import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPush;
+
+/**
+ * Created by arrizabalaga on 3/02/16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class PushOKTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
+            LoginActivity.class);
+
+    @BeforeClass
+    public static void setupClass(){
+        PopulateDB.wipeDatabase();
+    }
+
+    @Before
+    public void setup(){
+        PopulateDB.wipeDatabase();
+    }
+
+    @Test
+    public void pushWithPermissionsDoesPush(){
+        login(HNQIS_DEV_STAGING, TEST_USERNAME_WITH_PERMISSION, TEST_PASSWORD_WITH_PERMISSION);
+        waitForPull(20);
+        startSurvey(1, 6);
+        fillSurvey(7, "No");
+        Long idSurvey=markInProgressAsCompleted();
+
+        //then: Survey is pushed (UID)
+        Survey survey=waitForPush(20,idSurvey);
+        assertTrue(survey.getEventUid()!=null);
+
+        //then: Row is gone
+        onView(withId(R.id.score)).check(doesNotExist());
+    }
+
+}
