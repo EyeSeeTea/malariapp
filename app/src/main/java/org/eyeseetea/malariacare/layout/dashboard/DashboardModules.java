ce7deb4cc482406068df417ed5ee21d0e57bc299
idelcano <ignaciodelcano@gmail.com>
2016-02-25 18:30:52 +0100

    Added the DashboardModules and the Assess Improve Monitor and Plan modules

---
+++ app/src/main/java/org/eyeseetea/malariacare/layout/dashboard/DashboardModules.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2016.
+ *
+ * This file is part of QA App.
+ *
+ *  Health Network QIS App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Health Network QIS App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eyeseetea.malariacare.layout.dashboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by idelcano on 25/02/2016.
+ */
+public class DashboardModules {
+    private List<IModule> modules;
+    public static int VERTICAL=0;
+    public static int HORIZONTAL=1;
+    private int style;
+    private int layout;
+
+    public DashboardModules(int layout, int style){
+        this.layout=layout;
+        this.style=style;
+        modules=new ArrayList<>();
+    }
+
+    public  void addModule(IModule module){
+        modules.add(module);
+    }
+
+
+    public  void removeModule(IModule module){
+        modules.remove(module);
+    }
+
+    public void showModules(){
+        if(style==VERTICAL){
+
+        }
+        else if(style==HORIZONTAL){
+
+        }
+    }
+
+    public List<IModule> getModules() {
+        return modules;
+    }
+
+    public int getStyle() {
+        return style;
+    }
+
+    public void setStyle(int style) {
+        this.style = style;
+    }
+
+    public int getLayout() {
+        return layout;
+    }
+
+    public void setLayout(int layout) {
+        this.layout = layout;
+    }
+}
---
+++ app/src/main/java/org/eyeseetea/malariacare/layout/dashboard/ModulePlan.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016.
+ *
+ * This file is part of QA App.
+ *
+ *  Health Network QIS App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Health Network QIS App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eyeseetea.malariacare.layout.dashboard;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.graphics.drawable.Drawable;
+
+import org.eyeseetea.malariacare.R;
+import org.eyeseetea.malariacare.database.utils.PreferencesState;
+import org.eyeseetea.malariacare.fragments.PlannedFragment;
+
+/**
+ * Created by idelcano on 25/02/2016.
+ */
+public class ModulePlan extends AModule {
+    PlannedFragment plannedFragment;
+
+    public ModulePlan(int layout, boolean visible) {
+        this.layout=layout;
+        this.icon= PreferencesState.getInstance().getContext().getResources().getDrawable(R.drawable.tab_plan);
+        this.name= PreferencesState.getInstance().getContext().getResources().getString(R.string.tab_tag_plan);
+        this.animatorInLeft= R.animator.anim_slide_in_left;
+        this.animatorOutLeft= R.animator.anim_slide_out_left;
+        this.animatorInRight=R.animator.anim_slide_in_right;
+        this.animatorOutRight=R.animator.anim_slide_out_right;
+        this.fragmentTransactionLeft=fragmentTransactionLeft;
+        this.fragmentTransactionRight=fragmentTransactionRight;
+        this.visible=visible;
+        plannedFragment = new PlannedFragment();
+    }
+
+    @Override
+    public Fragment getFragment() {
+        return plannedFragment;
+    }
+
+    @Override
+    public void reloadData() {
+        plannedFragment.reloadPlannedItems();
+    }
+}
