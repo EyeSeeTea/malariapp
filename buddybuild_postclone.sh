f794bb265e1a45831292882f17d27e904652c8ac
ifoche <nacho.foche@gmail.com>
2016-02-26 10:41:11 +0100

    Revert "simplifying buddybuild postclone script"
    This reverts commit 105017490984dab7c6e6b103fca7aca71d919683.

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cd sdk
-git checkout 2.22_EyeSeeTea
-cd -
+rm -rf DBFlowORM
+git clone git@github.com:EyeSeeTea/DBFlow.git DBFlowORM
+rm -rf sdk
+git clone -b 2.22_EyeSeeTea git@github.com:EyeSeeTea/dhis2-android-sdk.git sdk
 cp -a DBFlowORM sdk
