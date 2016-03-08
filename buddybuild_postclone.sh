29ccca59dc9c718a3345d351e67105f86ec3e542
idelcano <ignaciodelcano@gmail.com>
2016-03-08 22:08:46 +0100

    Added last commit in buddy build script(fixed)

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 filePath="app\src\main\res\raw\commit.txt"
-commit=git log  -1 --pretty=format:"%h"
+commit=`git log  -1 --pretty=format:"%h"`
 echo $commit>$filePath
 
 cd sdk
