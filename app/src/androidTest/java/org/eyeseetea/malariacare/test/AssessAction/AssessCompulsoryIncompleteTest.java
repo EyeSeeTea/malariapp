f8b16435e385572089ab627c4d284aa63e95b508
idelcano <ignaciodelcano@gmail.com>
2016-03-14 16:26:03 +0100

    Separe the test in different files.(For be a 100% independent test)

--- app/src/androidTest/java/org/eyeseetea/malariacare/test/AssessAction/AssessCompulsoryIncompleteTest.java
+++ app/src/androidTest/java/org/eyeseetea/malariacare/test/AssessAction/AssessCompulsoryIncompleteTest.java
@@ -19,10 +19,7 @@
 
 package org.eyeseetea.malariacare.test.AssessAction;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import com.raizlabs.android.dbflow.sql.language.Delete;
@@ -31,38 +28,30 @@ import org.eyeseetea.malariacare.LoginActivity;
 import org.eyeseetea.malariacare.R;
 import org.eyeseetea.malariacare.database.model.Survey;
 import org.eyeseetea.malariacare.database.utils.Session;
-import org.eyeseetea.malariacare.test.utils.ElapsedTimeIdlingResource;
 import org.eyeseetea.malariacare.test.utils.SDKTestUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertTrue;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.DEFAULT_WAIT_FOR_PULL;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.HNQIS_DEV_CI;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_PASSWORD_WITH_PERMISSION;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_USERNAME_WITH_PERMISSION;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.fillCompulsorySurvey;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.fillSurvey;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.login;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.startSurvey;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPull;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPush;
 
 /**
  * Created by idelcano on 14/03/2016.
  */
-@RunWith(AndroidJUnit4.class)
-public class AssessCompulsoryTest {
-
+public class AssessCompulsoryIncompleteTest {
     private static final String TAG = "AssessActionTest";
 
     @Rule
@@ -82,42 +71,29 @@ public class AssessCompulsoryTest {
         SDKTestUtils.exitApp();
     }
 
+    @After
+    public void deleteSurveys() throws Exception{
+        Delete.tables(Survey.class);
+    }
+
     @Test
-    public void assessCompulsoryComplete() {
+    public void assessCompulsoryIncomplete() {
         //GIVEN
         if(LoginActivity.class.equals(SDKTestUtils.getActivityInstance().getClass())) {
             login(HNQIS_DEV_CI, TEST_USERNAME_WITH_PERMISSION, TEST_PASSWORD_WITH_PERMISSION);
             waitForPull(DEFAULT_WAIT_FOR_PULL);
         }
         startSurvey(SDKTestUtils.TEST_FACILITY_1_IDX, SDKTestUtils.TEST_FACILITY_1_IDX);
-        fillCompulsorySurvey(13, "Yes");
+        fillSurvey(12, "Yes");
 
         //WHEN
-        Espresso.pressBack();
-        //select continue later and go to assess
-        onView(withText(R.string.dialog_continue_later_option)).perform(click());
-
-        //THEN
-        onView(withText(R.string.dashboard_info_ready_to_upload)).check(matches(isDisplayed()));
-
-
-        //WHEN
-        Long idSurvey = SDKTestUtils.editSurvey();
-        IdlingResource idlingResource = new ElapsedTimeIdlingResource(5 * 1000);
-        Espresso.registerIdlingResources(idlingResource);
-        Espresso.pressBack();
-        Espresso.unregisterIdlingResources(idlingResource);
-        //select complete survey on exit
-        onView(withText(R.string.dialog_complete_option)).perform(click());
-        //confirm(are you sure?)
-        onView(withText(android.R.string.yes)).perform(click());
-        //info dialog
-        onView(withText(android.R.string.ok)).perform(click());
-
-        //THEN
-        //then: Survey is pushed (UID)
+        Long idSurvey = SDKTestUtils.markAsCompleteCompulsory();
         Log.d(TAG, "Session user ->" + Session.getUser());
-        Survey survey = waitForPush(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH, idSurvey);
-        assertTrue(survey.isCompleted() || survey.isSent());
+        Survey survey = Survey.findById(idSurvey);
+
+        //THEN
+        onView(withText(R.string.accept)).perform(click());
+        assertTrue(survey.isInProgress());
     }
+
 }
