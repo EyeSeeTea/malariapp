173e18729b85ebbb0099c5394504a83c9f14da85
idelcano <ignaciodelcano@gmail.com>
2016-03-18 01:31:15 +0100

    try again buddybuild script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-filePath="app\src\main\res\raw\commit.txt"
+filePath="app/src/main/res/raw/commit.txt"
 echo "execute commit command in path $filePath"
 commit=`git log -1 HEAD --format=%H`
 echo $commit
+cat $filePath
 echo $commit > $filePath
+cat $filePath
 
 cd sdk
 git checkout 2.22_EyeSeeTea
