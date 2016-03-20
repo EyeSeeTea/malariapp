cd46f7d72542ab90e65fb369848d07f69868abaa
idelcano <ignaciodelcano@gmail.com>
2016-03-21 00:16:04 +0100

    other try

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -5,11 +5,13 @@ echo "echo \" running post-checkout hook \"" >> .git/hooks/post-checkout
 echo "bash generate_last_commit.sh" >> .git/hooks/post-checkout
 echo "reload actual branch"
 branch=`git rev-parse --abbrev-ref HEAD`
-git checkout -- buddybuild_postclone.sh
-git checkout development
-git checkout $branch
 echo "git checkout"
 cd sdk
 git checkout 2.22_EyeSeeTea
 cd -
 cp -a DBFlowORM sdk
+
+git checkout -- buddybuild_postclone.sh
+git checkout development
+git checkout $branch
+bash .git/hooks/post-checkout
