95eb6204bd2dd192b24e73d24f31cf08b4a82bd7
idelcano <ignaciodelcano@gmail.com>
2016-03-20 21:49:33 +0100

    Fix buddybuild_postclone script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+echo "Creating git post-checkout hook"
+touch .git/hooks/post-checkout
 echo "#!/usr/bin/env bash" >> .git/hooks/post-checkout
+echo "echo \" running post-checkout hook \"" >> .git/hooks/post-checkout
 echo "bash generate_last_commit.sh" >> .git/hooks/post-checkout
+echo "show hook post-checkout content"
+cat .git/hooks/post-checkout
 git checkout
+echo "reload actual branch"
+
 cd sdk
 git checkout 2.22_EyeSeeTea
 cd -
