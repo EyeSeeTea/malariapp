71513be0117e7db8269a1ec0ad5f3aa42da992df
idelcano <ignaciodelcano@gmail.com>
2016-03-20 21:54:57 +0100

    fixed buddybuild script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 echo "Creating git post-checkout hook"
 touch .git/hooks/post-checkout
-echo "#!/usr/bin/env bash" >> .git/hooks/post-checkout
+echo "#!/usr/bash" >> .git/hooks/post-checkout
 echo "echo \" running post-checkout hook \"" >> .git/hooks/post-checkout
 echo "bash generate_last_commit.sh" >> .git/hooks/post-checkout
+echo "ls" >> .git/hooks/post-checkout
 echo "show hook post-checkout content"
 cat .git/hooks/post-checkout
-git checkout
 echo "reload actual branch"
+git checkout
 
 cd sdk
 git checkout 2.22_EyeSeeTea
