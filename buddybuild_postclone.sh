3ceb5f1038e83fceb95ea5da93916ad50ebf0c45
idelcano <ignaciodelcano@gmail.com>
2016-03-20 23:04:57 +0100

    Run post-checkout hook without checkout in buddybuild

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-cd sdk
-git checkout 2.22_EyeSeeTea
-cd -
-cp -a DBFlowORM sdk
-
 echo "Creating git post-checkout hook"
 echo "#!/usr/bin/env bash" >> .git/hooks/post-checkout
 echo "echo \" running post-checkout hook \"" >> .git/hooks/post-checkout
 echo "bash generate_last_commit.sh" >> .git/hooks/post-checkout
 echo "reload actual branch"
-echo "git checkout"
-git checkout -- buddybuild_postclone.sh
+branch=`git rev-parse --abbrev-ref HEAD`
+git checkout $branch
+echo "git checkout(simulated)"
+bash .git/hooks/post-checkout
+
+cd sdk
+git checkout 2.22_EyeSeeTea
+cd -
+cp -a DBFlowORM sdk
