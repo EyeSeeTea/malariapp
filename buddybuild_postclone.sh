fb0e627dca884a45687e081e00c55f71ec3de8e1
idelcano <ignaciodelcano@gmail.com>
2016-03-20 22:52:11 +0100

    Trying to run hook post-checkout in buddybuild

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,12 +3,8 @@ echo "Creating git post-checkout hook"
 echo "#!/usr/bin/env bash" >> .git/hooks/post-checkout
 echo "echo \" running post-checkout hook \"" >> .git/hooks/post-checkout
 echo "bash generate_last_commit.sh" >> .git/hooks/post-checkout
-echo "reload actual branch"
-branch=`git rev-parse --abbrev-ref HEAD`
-echo $branch
 echo "git checkout"
-echo "file">>file
-git checkout -- .
+git checkout HEAD
 
 
 
