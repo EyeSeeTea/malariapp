983724eed451a8f87d4fe2a82ac983edee6ebf0f
idelcano <ignaciodelcano@gmail.com>
2016-03-21 09:14:59 +0100

    Show all asssessments in planningtab filter from the same string resource as others filters

--- app/src/main/java/org/eyeseetea/malariacare/fragments/PlannedFragment.java
+++ app/src/main/java/org/eyeseetea/malariacare/fragments/PlannedFragment.java
@@ -77,7 +77,7 @@ public class PlannedFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState){
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        programDefaultOption = new Program(getResources().getString(R.string.all_assessment).toUpperCase());
+        programDefaultOption = new Program(getResources().getString(R.string.filter_all_org_assessments_upper));
     }
 
     @Override
