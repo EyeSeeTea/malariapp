2de459da5291e5e0016947a017ae61e8fa0f3bd6
ifoche <nacho.foche@gmail.com>
2016-03-22 13:01:59 +0100

    shell script corrections

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/env bash
-bash generate_last_commit.sh
-#echo "Creating git post-checkout hook"
-#echo "#!/usr/bin/env bash" >> .git/hooks/post-checkout
-#echo "echo \" running post-checkout hook \"" >> .git/hooks/post-checkout
-#echo "bash generate_last_commit.sh" >> .git/hooks/post-checkout
-#echo "reload actual branch"
-#branch=`git rev-parse --abbrev-ref HEAD`
-#echo "git checkout"
+#!/bin/sh
+
+# Definitions
+gitPath=$(git rev-parse --show-toplevel)
+postCheckoutFile=${gitPath}/.git/hooks/post-checkout
+
+# Generate last commit
+sh ${gitPath}/generate_last_commit.sh
+
+# Use the EST SDK branch
 cd sdk
 git checkout 2.22_EyeSeeTea
 cd -
 cp -a DBFlowORM sdk
 
-#git checkout -- buddybuild_postclone.sh
-#git checkout development
-#git checkout $branch
