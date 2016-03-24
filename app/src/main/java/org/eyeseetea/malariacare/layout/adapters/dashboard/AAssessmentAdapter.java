2f5a060fa776cf065f993aeaf5778aef00ad4dd8
idelcano <ignaciodelcano@gmail.com>
2016-03-24 17:45:42 +0100

    Hidden the facility name

--- app/src/main/java/org/eyeseetea/malariacare/layout/adapters/dashboard/AAssessmentAdapter.java
+++ app/src/main/java/org/eyeseetea/malariacare/layout/adapters/dashboard/AAssessmentAdapter.java
@@ -96,6 +96,8 @@ public abstract class AAssessmentAdapter extends ADashboardAdapter implements ID
             ((CustomTextView) rowView.findViewById(R.id.score)).setText(getStatus(survey));
         }
 
+        //Hide all the facility name, waiting for other way to draw the facility names.
+        showNextFacilityName=false;
         // show facility name (or not) and write survey type name
         if (!showNextFacilityName) {
             facilityName.setVisibility(View.GONE);
