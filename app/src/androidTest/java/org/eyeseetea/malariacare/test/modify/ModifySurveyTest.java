5ede64c17a4b748faee6ca980ef29c1a233945f2
idelcano <ignaciodelcano@gmail.com>
2016-04-04 15:59:42 +0200

    Fix test "ModifySurveyTest"

--- app/src/androidTest/java/org/eyeseetea/malariacare/test/modify/ModifySurveyTest.java
+++ app/src/androidTest/java/org/eyeseetea/malariacare/test/modify/ModifySurveyTest.java
@@ -17,7 +17,7 @@
  *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.eyeseetea.malariacare.test.path;
+package org.eyeseetea.malariacare.test.modify;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
@@ -25,15 +25,14 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
-import org.eyeseetea.malariacare.BuildConfig;
 import org.eyeseetea.malariacare.LoginActivity;
 import org.eyeseetea.malariacare.R;
 import org.eyeseetea.malariacare.database.model.Survey;
+import org.eyeseetea.malariacare.database.model.Value;
 import org.eyeseetea.malariacare.database.utils.Session;
 import org.eyeseetea.malariacare.test.utils.ElapsedTimeIdlingResource;
 import org.eyeseetea.malariacare.test.utils.SDKTestUtils;
-import org.eyeseetea.malariacare.utils.Constants;
-import org.hisp.dhis.android.sdk.persistence.models.DataValue;
+import org.hisp.dhis.android.sdk.persistence.models.Event;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
@@ -42,26 +41,19 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertTrue;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.DEFAULT_WAIT_FOR_PULL;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.HNQIS_DEV_CI;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_PASSWORD_WITH_PERMISSION;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.TEST_USERNAME_WITH_PERMISSION;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.fillPathSurvey;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.fillSurvey;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.login;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.markInProgressAsCompleted;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.startSurvey;
-import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPull;
 import static org.eyeseetea.malariacare.test.utils.SDKTestUtils.waitForPush;
 
 /**
  * Created by idelcano on 25/03/2016.
  */
 @RunWith(AndroidJUnit4.class)
-public class PathSurveyTest {
+public class ModifySurveyTest {
 
     private static final String TAG="PushOKTest";
 
@@ -76,7 +68,7 @@ public class PathSurveyTest {
         //force init go to logging activity.
         //SDKTestUtils.goToLogin();
         //set the test limit( and throw exception if the time is exceded)
-        SDKTestUtils.setTestTimeoutSeconds(SDKTestUtils.DEFAULT_TEST_TIME_LIMIT);
+        SDKTestUtils.setTestTimeoutSeconds(SDKTestUtils.DEFAULT_TEST_TIME_LIMIT*2);
     }
 
     @AfterClass
@@ -86,49 +78,75 @@ public class PathSurveyTest {
 
     @Test
     public void pushAndPathSurveyTest(){
+        //Given
         //if eds
         //if(BuildConfig.FLAVOR=="eds") {}
         //login(HNQIS_DEV_CI, TEST_USERNAME_WITH_PERMISSION, TEST_PASSWORD_WITH_PERMISSION);
         //waitForPull(DEFAULT_WAIT_FOR_PULL);
-        startSurvey(SDKTestUtils.TEST_FACILITY_1_IDX, SDKTestUtils.TEST_FAMILY_PLANNING_IDX);
+        int modifcatedOptions = 7;
+        //The first is "Real/Simulation" and it is not saved
+        int expectedOptions = 6;
+        String eventUid="";
+        startSurvey(SDKTestUtils.TEST_FACILITY_1_IDX, SDKTestUtils.TEST_CC);
 
-
-        onView(withId(R.string.create)).perform(click());
-
-        long eventCount = SDKTestUtils.getEventCount();
-        fillSurvey(17, "yes");
-        Long idSurvey=markInProgressAsCompleted();
-        //then: Survey is pushed (UID)
-        Log.d(TAG, "Session user ->" + Session.getUser());
-        Survey survey=waitForPush(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH*1000,idSurvey);
-
-
-
-        IdlingResource idlingResource = new ElapsedTimeIdlingResource(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH *1000);
+        IdlingResource idlingResource = new ElapsedTimeIdlingResource(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 500);
         Espresso.registerIdlingResources(idlingResource);
-        Log.d(TAG, survey.toString());
+        onView(withText(R.string.cancel)).perform(click());
+        onView(withId(R.id.create_form_button)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource);
 
+        idlingResource = new ElapsedTimeIdlingResource(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 500);
+        Espresso.registerIdlingResources(idlingResource);
+        onView(withText(R.string.create)).perform(click());
+        Espresso.unregisterIdlingResources(idlingResource);
+
+
+        long eventCount = SDKTestUtils.getEventCount();
+        fillSurvey(17, "Yes");
+        Long idSurvey=markInProgressAsCompleted();
+
+        //When
+        Log.d(TAG, "Session user ->" + Session.getUser());
+        Survey survey=waitForPush(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 1000, idSurvey);
+        eventUid=survey.getEventUid();
+        //then: Survey is pushed (UID)
         assertTrue(survey.getEventUid() != null);
         assertTrue(eventCount + 1 == SDKTestUtils.getEventCount());
 
         //WHEN
-        startSurvey(SDKTestUtils.TEST_FACILITY_1_IDX, SDKTestUtils.TEST_FAMILY_PLANNING_IDX);
-        onView(withId(R.string.patch)).perform(click());
+        startSurvey(SDKTestUtils.TEST_FACILITY_1_IDX, SDKTestUtils.TEST_CC);
 
-        //then: Row is gone
+        idlingResource = new ElapsedTimeIdlingResource(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 1000);
 
-        fillPathSurvey(7, 7, "no");
-        idSurvey=markInProgressAsCompleted();
-        survey=waitForPush(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 1000, idSurvey);
-        idlingResource = new ElapsedTimeIdlingResource(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH *1000);
         Espresso.registerIdlingResources(idlingResource);
-        Log.d(TAG, survey.toString());
+        onView(withText(R.string.modify)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource);
 
-        assertTrue(survey.getEventUid() != null);
-        assertTrue(eventCount + 1 == SDKTestUtils.getEventCount());
+        fillSurvey(modifcatedOptions, "No");
 
-        onView(withId(R.id.score)).check(doesNotExist());
+        idSurvey=markInProgressAsCompleted();
+        survey=waitForPush(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 1000, idSurvey);
+
+        //THEN
+        assertTrue(survey.getEventUid() != null);
+        idlingResource = new ElapsedTimeIdlingResource(SDKTestUtils.DEFAULT_WAIT_FOR_PUSH * 10000);
+        Espresso.registerIdlingResources(idlingResource);
+        assertTrue(survey.getEventUid().equals(eventUid));
+        Espresso.unregisterIdlingResources(idlingResource);
+        if(survey.isCompleted()) {
+            int modificatedValues = 0;
+            for (Value value : survey.getValues()) {
+                if (value.getUploadedDate().after(survey.getUploadedDate())) {
+                    modificatedValues++;
+                }
+            }
+            assertTrue(modificatedValues == expectedOptions);
+        }
+        else if (survey.isSent()){
+            Event event=SDKTestUtils.getEvent(survey.getEventUid());
+            assertTrue(event.getDataValues().size() == expectedOptions);
+        }
+
+        assertTrue(eventCount + 1 == SDKTestUtils.getEventCount());
     }
 }
