5a4b3bd8cf162f4472794bd0fe4f0ed937d75b79
idelcano <ignaciodelcano@gmail.com>
2016-04-04 18:41:20 +0200

    Remove eds code from hnqis variant

--- app/src/hnqis/java/org/eyeseetea/malariacare/utils/VariantSpecificUtils.java
+++ app/src/hnqis/java/org/eyeseetea/malariacare/utils/VariantSpecificUtils.java
@@ -24,36 +24,21 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.text.method.LinkMovementMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.raizlabs.android.dbflow.sql.builder.Condition;
-import com.raizlabs.android.dbflow.sql.language.Select;
-
-import org.eyeseetea.malariacare.BaseActivity;
 import org.eyeseetea.malariacare.BuildConfig;
 import org.eyeseetea.malariacare.DashboardActivity;
 import org.eyeseetea.malariacare.R;
-import org.eyeseetea.malariacare.database.iomodules.dhis.importer.PullController;
 import org.eyeseetea.malariacare.database.iomodules.dhis.importer.models.EventExtended;
 import org.eyeseetea.malariacare.database.model.OrgUnit;
-import org.eyeseetea.malariacare.database.model.Survey;
-import org.eyeseetea.malariacare.database.model.Survey$Table;
 import org.eyeseetea.malariacare.database.model.TabGroup;
 import org.eyeseetea.malariacare.database.utils.PreferencesState;
-import org.eyeseetea.malariacare.network.PushClient;
-import org.hisp.dhis.android.sdk.persistence.models.Event;
-
-import java.util.List;
+import org.eyeseetea.malariacare.network.PullClient;
 
 /**
  * Created by nacho on 28/03/16.
@@ -90,101 +75,9 @@ public class VariantSpecificUtils{
         //now that the dialog is set up, it's time to show it
         dialog.show();
     }
-    public static OrgUnit orgUnit;
-    public static TabGroup tabGroup;
-    public static DashboardActivity activity;
+
     public void createNewSurvey(final OrgUnit orgUnit, final TabGroup tabGroup) {
-        this.orgUnit=orgUnit;
-        this.tabGroup=tabGroup;
-        activity = ((DashboardActivity) DashboardActivity.dashboardActivity);
-        askCreateOrModify();
-        //reloadAllEvents();
-    }
-
-    private void askCreateOrModify() {
-
-    RefreshEvents refreshEvents= new RefreshEvents();
-        refreshEvents.execute(DashboardActivity.dashboardActivity);
-    }
-
-private PushClient getPushClient(Context context){
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String user=sharedPreferences.getString(context.getString(R.string.dhis_user), "");
-    String password=sharedPreferences.getString(context.getString(R.string.dhis_password), "");
-    return new PushClient(context, user,password);
-}
-    @NonNull
-    private DashboardActivity reloadAllEvents() {
-        //activity.createNewSurvey(orgUnit, tabGroup);
-        RefreshEvents refreshEvents=new RefreshEvents();
-        refreshEvents.execute(activity.getApplicationContext());
-        return activity;
-    }
-
-
-    private class RefreshEvents extends AsyncTask<Context, Void, Void> {
-        Survey survey;
-        @Override
-        protected Void doInBackground(Context... params) {
-            //sleep for wait the ontab change
-            //PullController.refreshEvents(PreferencesState.getInstance().getContext());
-
-            PushClient pushClient = getPushClient(DashboardActivity.dashboardActivity);
-            //Push  data
-            Event lastEvent=pushClient.getLastEvent(orgUnit, tabGroup.getProgram());
-            survey=Survey.getLastSurvey(orgUnit, tabGroup);
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            // Dismiss the progress dialog
-            new AlertDialog.Builder(activity)
-                    .setTitle("")
-                    .setMessage(String.format(PreferencesState.getInstance().getContext().getResources().getString(R.string.create_or_patch), EventExtended.format(survey.getCompletionDate(), EventExtended.DHIS2_DATE_FORMAT ))+survey.getEventUid())
-                    .setPositiveButton((R.string.create), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface arg0, int arg1) {
-                            activity.createNewSurvey(orgUnit, tabGroup);
-                        }
-                    })
-                    .setNeutralButton((R.string.patch), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface arg0, int arg1) {
-                            activity.patchSurvey(orgUnit, tabGroup);
-                        }
-                    })
-                    .setNegativeButton((R.string.cancel), null)
-                    .setCancelable(true)
-                    .create().show();
-            if (progressDialog.isShowing()) {
-                progressDialog.dismiss();
-            }
-        }
-        ProgressDialog progressDialog;
-        @Override
-        protected void onPreExecute() {
-
-            progressDialog = new ProgressDialog(DashboardActivity.dashboardActivity);
-            progressDialog.setMessage("Loading...");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onCancelled() {
-
-            super.onCancelled();
-            progressDialog.dismiss();
-            Toast toast = Toast.makeText(DashboardActivity.dashboardActivity,
-                    "Error is occured due to some problem", Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.TOP, 25, 400);
-            toast.show();
-
-        }
+        final DashboardActivity activity = ((DashboardActivity) DashboardActivity.dashboardActivity);
+        activity.createNewSurvey(orgUnit, tabGroup);
     }
 }
