124bdfe229c3af299b7bf80fd8dfce8d035d510c
idelcano <ignaciodelcano@gmail.com>
2016-04-08 09:15:22 +0200

    Saved the event creation date after push

--- app/src/main/java/org/eyeseetea/malariacare/database/iomodules/dhis/exporter/ConvertToSDKVisitor.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/iomodules/dhis/exporter/ConvertToSDKVisitor.java
@@ -333,6 +333,7 @@ public class ConvertToSDKVisitor implements IConvertToSDKVisitor {
                 iEvent.delete();
                 Log.d(TAG, "PUSH process...Fail pushing survey: " + iSurvey.getId_survey());
             }else{
+                PushController.getInstance().saveCreationDateInSDK(surveys);
                 iSurvey.setStatus(Constants.SURVEY_SENT);
                 iSurvey.saveMainScore();
                 iSurvey.save();
