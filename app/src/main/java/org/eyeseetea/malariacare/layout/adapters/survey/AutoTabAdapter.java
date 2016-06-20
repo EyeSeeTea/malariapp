e6a06fa594d4b8d1e04114717849c1dbd1a61786
idelcano <ignaciodelcano@gmail.com>
2016-06-20 12:52:18 +0200

    Added clear button in DatePickerListener

--- app/src/main/java/org/eyeseetea/malariacare/layout/adapters/survey/AutoTabAdapter.java
+++ app/src/main/java/org/eyeseetea/malariacare/layout/adapters/survey/AutoTabAdapter.java
@@ -612,43 +612,7 @@ public class AutoTabAdapter extends ATabAdapter {
         }
     }
 
-    private class DatePickerListener implements Button.OnClickListener{
 
-        private AutoTabViewHolder viewHolder;
-        private Question question;
-        private Calendar calendar = Calendar.getInstance();
-
-        public DatePickerListener(Question question, AutoTabViewHolder viewHolder){
-            this.question = question;
-            this.viewHolder = viewHolder;
-        }
-
-
-        @Override
-        public void onClick(final View v) {
-            if (!v.isShown()){
-                return;
-            }
-            new DatePickerDialog(
-                    AutoTabAdapter.this.getContext(), new DatePickerDialog.OnDateSetListener() {
-                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    Calendar newCalendar = Calendar.getInstance();
-                    newCalendar.set(year, monthOfYear, dayOfMonth);
-                    Date newScheduledDate = newCalendar.getTime();
-                    ((CustomButton)v).setText(AUtils.formatDate(newScheduledDate));
-                    ReadWriteDB.saveValuesText(question, AUtils.formatDate(newCalendar.getTime()), module);
-                }
-
-            },calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
-                    .setButton(DialogInterface.BUTTON_NEUTRAL, "Name", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                ((CustomButton)v).setText((null));
-                ReadWriteDB.deleteValue(question, module);
-            }
-        });
-        }
-    }
 
 
 
@@ -734,4 +698,47 @@ public class AutoTabAdapter extends ATabAdapter {
         }
     }
 
+    public class DatePickerListener implements Button.OnClickListener {
+
+        private AutoTabViewHolder viewHolder;
+        private Question question;
+        private Calendar calendar = Calendar.getInstance();
+
+        public DatePickerListener(Question question, AutoTabViewHolder viewHolder) {
+            this.question = question;
+            this.viewHolder = viewHolder;
+        }
+
+
+        @Override
+        public void onClick(final View v) {
+            if (!v.isShown()) {
+                return;
+            }
+            final boolean[] cleared = {false};
+            DatePickerDialog.OnDateSetListener datepickerlistener = new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    Calendar newCalendar = Calendar.getInstance();
+                    newCalendar.set(year, monthOfYear, dayOfMonth);
+                    Date newScheduledDate = newCalendar.getTime();
+                    if(!cleared[0]) {
+                        ((CustomButton) v).setText( AUtils.formatDate(newCalendar.getTime()));
+                        ReadWriteDB.saveValuesText(question, AUtils.formatDate(newCalendar.getTime()), module);
+                    }
+                    cleared[0] =false;
+                }
+            };
+            Calendar newCalendar = Calendar.getInstance();
+            DatePickerDialog datePickerDialog = new DatePickerDialog(AutoTabAdapter.this.getContext(), datepickerlistener, newCalendar.get(Calendar.YEAR),newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+            datePickerDialog.setButton(DialogInterface.BUTTON_NEUTRAL, PreferencesState.getInstance().getContext().getResources().getString(R.string.clear), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ReadWriteDB.deleteValue(question, module);
+                    cleared[0] =true;
+                    ((CustomButton) v).setText("");
+                }
+            });
+            datePickerDialog.show();
+        }
+    }
 }
