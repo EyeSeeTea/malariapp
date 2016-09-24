37fb33fbac1b63dec737a6b488c61e4487eedcf8
idelcano <ignaciodelcano@gmail.com>
2016-09-24 07:25:53 +0200

    when the mobile is rotated continues the video in the current position

--- app/src/main/java/org/eyeseetea/malariacare/VideoActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/VideoActivity.java
@@ -2,6 +2,7 @@ package org.eyeseetea.malariacare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -9,24 +10,40 @@ import android.widget.VideoView;
  * Created by arrizabalaga on 30/05/16.
  */
 public class VideoActivity extends Activity {
+    public static final String TAG=".VideoActivity";
 
     public static final String VIDEO_PATH_PARAM="videoPathParam";
-
+    VideoView mVideoView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_activity);
 
         //Displays a video file.
-        MediaController mediaController=new MediaController(VideoActivity.this);
-        VideoView mVideoView = (VideoView)findViewById(R.id.videoview);
-        String videoPathParam=getIntent().getStringExtra(VIDEO_PATH_PARAM);
+        MediaController mediaController = new MediaController(VideoActivity.this);
+        mVideoView = (VideoView)findViewById(R.id.videoview);
+        String videoPathParam = getIntent().getStringExtra(VIDEO_PATH_PARAM);
         mVideoView.setVideoPath(videoPathParam);
         mVideoView.setMediaController(mediaController);
         mediaController.setAnchorView(mVideoView);
 
         mVideoView.requestFocus();
+        if (savedInstanceState != null) {
+            mVideoView.seekTo(savedInstanceState.getInt("video",0));
+        }
         mVideoView.start();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("video", mVideoView.getCurrentPosition());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState); 
+        mVideoView.seekTo(savedInstanceState.getInt("video",0));
     }
 }
 
