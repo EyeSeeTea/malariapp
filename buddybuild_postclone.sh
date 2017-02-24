58f3adda3777148d81557ce31cee35ace246bf47
ifoche <nacho.foche@gmail.com>
2017-02-24 23:14:12 +0100

    changed buddybuild script to point to 2.25 sdk versio

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -9,10 +9,7 @@ sh ${gitPath}/generate_last_commit.sh
 
 # Use the EST SDK branch
 cd sdk
-git checkout 2.22_EyeSeeTea
-cd -
-cd DBFlowORM
-git checkout update_to_gradle_2.0
+git checkout 2.25_EyeSeeTea
 cd -
 cp -a DBFlowORM sdk
 cp ${BUDDYBUILD_SECURE_FILES}/driveserviceprivatekey.json app/src/hnqis/res/raw/driveserviceprivatekey.json
