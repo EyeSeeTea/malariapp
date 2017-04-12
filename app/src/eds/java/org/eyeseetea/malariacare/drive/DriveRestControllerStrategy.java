5897b62b3909ad377380c39da74c5c73395eb193
idelcano <ignaciodelcano@gmail.com>
2017-04-12 08:27:39 +0200

    Remove imports

--- app/src/eds/java/org/eyeseetea/malariacare/drive/DriveRestControllerStrategy.java
+++ app/src/eds/java/org/eyeseetea/malariacare/drive/DriveRestControllerStrategy.java
@@ -19,28 +19,9 @@
 
 package org.eyeseetea.malariacare.drive;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.util.Log;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.services.drive.DriveScopes;
 
 import org.eyeseetea.malariacare.DashboardActivity;
-import org.eyeseetea.malariacare.R;
-import org.eyeseetea.malariacare.database.utils.PreferencesState;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 
 /**
  * Created by arrizabalaga on 28/05/16.
