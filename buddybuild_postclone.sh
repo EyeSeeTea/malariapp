958bef85c1be8ee139580ae57984242de40a41e3
ifoche <nacho.foche@gmail.com>
2017-11-01 18:53:55 +0100

    fix postclone buddybuild script to deploy 1.2 version again

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -9,7 +9,7 @@ sh ${gitPath}/generate_last_commit.sh
 
 # Use the EST SDK branch
 cd sdk
-git checkout 2.25_EyeSeeTea
+git checkout v1.2_hnqis
 cd -
 cp ${BUDDYBUILD_SECURE_FILES}/driveserviceprivatekey.json app/src/hnqis/res/raw/driveserviceprivatekey.json
 
