f7156517f0ccedf3bcdae75a767a28290977ad85
Idelcano <ignaciodelcano@gmail.com>
2017-12-14 22:14:57 +0100

    fix hnqis changes

--- app/src/hnqis/java/org/eyeseetea/malariacare/views/ActionBarStrategy.java
+++ app/src/hnqis/java/org/eyeseetea/malariacare/views/ActionBarStrategy.java
@@ -5,4 +5,7 @@ import org.eyeseetea.malariacare.data.database.model.SurveyDB;
 import org.eyeseetea.malariacare.layout.utils.LayoutUtils;
 
 public class ActionBarStrategy extends LayoutUtils {
+    public static void setActionBarForSurvey(DashboardActivity dashboardActivity, SurveyDB survey) {
+        LayoutUtils.setActionBarTitleForSurvey(dashboardActivity,survey);
+    }
 }
