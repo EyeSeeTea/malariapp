53033c77ee8da0d727f9cf2fe277f0baebe88181
idelcano <ignaciodelcano@gmail.com>
2018-03-09 05:07:16 +0100

    fix hnqis plan action fragment strategy

--- app/src/hnqis/java/org/eyeseetea/malariacare/strategies/PlanActionStyleStrategy.java
+++ app/src/hnqis/java/org/eyeseetea/malariacare/strategies/PlanActionStyleStrategy.java
@@ -1,18 +1,20 @@
 package org.eyeseetea.malariacare.strategies;
 
 import android.support.design.widget.FloatingActionButton;
+import android.widget.RelativeLayout;
 
 import org.eyeseetea.malariacare.R;
 import org.eyeseetea.malariacare.utils.Constants;
+import org.eyeseetea.sdk.presentation.views.DoubleRectChart;
 
 public class PlanActionStyleStrategy {
     public static void fabIcons(FloatingActionButton fabComplete, Integer status) {
         if (status.equals(Constants.SURVEY_IN_PROGRESS)) {
-            mFabComplete.setImageResource(R.drawable.ic_action_uncheck);
+            fabComplete.setImageResource(R.drawable.ic_action_uncheck);
         } else if (status == Constants.SURVEY_SENT) {
-            mFabComplete.setImageResource(R.drawable.ic_double_check);
+            fabComplete.setImageResource(R.drawable.ic_double_check);
         } else {
-            mFabComplete.setImageResource(R.drawable.ic_action_check);
+            fabComplete.setImageResource(R.drawable.ic_action_check);
         }
     }
 
