450a5e397eebd756463f148d82fbcf062c543641
idelcano <ignaciodelcano@gmail.com>
2018-03-26 19:19:55 +0200

    fix script

--- travis_script.sh
+++ travis_script.sh
@@ -3,6 +3,6 @@ for variant in "$@"
 do
 	upperCaseVariant="${variant^}"
 	echo "$upperCaseVariant"
-	param="test" + $upperCaseVariant + "DebugUnitTest"
-	./gradlew "$param" -i
+	param="test${upperCaseVariant}DebugUnitTest"
+	./gradlew $param -i
 done
