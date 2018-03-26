fc6eebe1a5c63843a5378e94be3818e4b2475e5f
idelcano <ignaciodelcano@gmail.com>
2018-03-26 21:59:14 +0200

    added travis script

---
+++ travis_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for variant in "$@"
+do
+	upperCaseVariant="${variant^}"
+	echo "$upperCaseVariant"
+	param="test${upperCaseVariant}DebugUnitTest"
+	commandResult=`./gradlew --no-daemon $param`
+	echo "command result $commandResult"
+	if [ $commandResult!="0" ]; then
+		exit 1
+	fi
+done
