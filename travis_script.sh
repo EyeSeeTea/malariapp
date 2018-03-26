865888a8ab072772dd7487de032d44a9c0b4d673
ignaciodelcano <ignaciodelcano@gmail.com>
2018-03-26 13:52:04 +0200

    test script

--- travis_script.sh
+++ travis_script.sh
@@ -1,5 +1,13 @@
 #!bin/bash
+for variant in "$@"
+do
+	upperCaseVariant="${variant^}"
+	echo "$upperCaseVariant"
+	param = "test" + $upperCaseVariant + "DebugUnitTest
+	./gradlew $param -i
+done
+
 for var in "$@"
 do
-   ./gradlew test"$var"DebugUnitTest --stacktrace
+  ./gradlew test"${var^}"DebugUnitTest --info
 done
