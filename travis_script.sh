6dc1ea24a70cf9440a200696f2e220688221bbca
idelcano <ignaciodelcano@gmail.com>
2018-03-26 20:07:54 +0200

    return error error if test scripts fails

--- travis_script.sh
+++ travis_script.sh
@@ -6,6 +6,10 @@ do
 	param="test${upperCaseVariant}DebugUnitTest"
 	./gradlew --no-daemon $param
 	param="test${upperCaseVariant}DebugTest"
+	if [ $? != 0 ]
+	then
+		exit $?
+	fi
 	#Instrumental test:
 	#./gradlew $param
 done
