9662ef020e8cb20edf8c159fdcceda5826a000cf
idelcano <ignaciodelcano@gmail.com>
2018-03-26 13:07:33 +0200

    fix travis script and add comment

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+echo "start travis before script"
 openssl aes-256-cbc -K $encrypted_4558f4131c88_key -iv $encrypted_4558f4131c88_iv -in driveserviceprivatekey.json.enc -out driveserviceprivatekey.json -d
+echo "decrypted file"
 cp driveserviceprivatekey.json app/src/hnqis/res/raw/driveserviceprivatekey.json
 cp driveserviceprivatekey.json app/src/eyeseetea/res/raw/driveserviceprivatekey.json
+echo "remove decrypted file"
 rm driveserviceprivatekey.json
+echo "continue"
 
