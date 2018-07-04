86eb81e33950bbf34185bc858cf525c08f5747e8
idelcano <ignaciodelcano@gmail.com>
2018-07-05 01:55:03 +0200

    update survey tests

--- app/src/test/java/org/eyeseetea/malariacare/domain/entity/SurveyShould.java
+++ app/src/test/java/org/eyeseetea/malariacare/domain/entity/SurveyShould.java
@@ -7,7 +7,7 @@ import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 
 public class SurveyShould {
 
@@ -16,8 +16,13 @@ public class SurveyShould {
 
     @Test
     public void create_survey_with_mandatory_fields(){
+        HashMap<String, Question> questions = new HashMap<>();
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
         Survey survey = Survey.createEmptySurvey(
-                "UID", "PROGRAM_UID", "ORG_UNIT_UID", "USER_UID");
+                "UID", "PROGRAM_UID", "ORG_UNIT_UID", "USER_UID", questions);
 
         Assert.assertNotNull(survey);
         Assert.assertTrue(survey.getUId().equals("UID"));
@@ -28,11 +33,20 @@ public class SurveyShould {
 
     @Test
     public void create_empty_survey(){
+        HashMap<String, Question> questions = new HashMap<>();
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
         Survey survey = Survey.createEmptySurvey(
-                "UID", "PROGRAM_UID", "ORG_UNIT_UID", "USER_UID");
+                "UID", "PROGRAM_UID", "ORG_UNIT_UID", "USER_UID", questions);
 
         Assert.assertNotNull(survey);
         Assert.assertNotNull(survey.getCreationDate());
+        Assert.assertTrue(survey.getSurveyAnsweredRatio().getTotal()==2);
+        Assert.assertTrue(survey.getSurveyAnsweredRatio().getTotalCompulsory()==2);
+        Assert.assertTrue(survey.getSurveyAnsweredRatio().getAnswered()==0);
+        Assert.assertTrue(survey.getSurveyAnsweredRatio().getCompulsoryAnswered()==0);
         Assert.assertTrue(survey.getStatus().equals(SurveyStatus.IN_PROGRESS));
     }
 
@@ -42,10 +56,15 @@ public class SurveyShould {
         Date uploadDate = new Date();
         Date scheduledDate = new Date();
         Date completionDate = new Date();
-        List<QuestionValue> values = new ArrayList<>();
+        HashMap<String, QuestionValue> values = new HashMap<>();
+        HashMap<String, Question> questions = new HashMap<>();
         Score score = new Score("ScoreUId", 100.0f);
-        values.add(QuestionValue.createSimpleValue("UId", "value"));
-        values.add(QuestionValue.createOptionValue("UId2", "optionUId", "value2"));
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
+        values.put("QuestionUID1", QuestionValue.createSimpleValue("UId", "value"));
+        values.put("QuestionUID2", QuestionValue.createOptionValue("UId2", "optionUId", "value2"));
 
         Survey survey = Survey.createSentSurvey("UID", "PROGRAM_UID", "ORG_UNIT_UID",
                 "USER_UID", creationDate, uploadDate, scheduledDate, completionDate, values, score);
@@ -56,7 +75,7 @@ public class SurveyShould {
         Assert.assertTrue(survey.getCompletionDate().equals(completionDate));
         Assert.assertTrue(survey.getScheduledDate().equals(scheduledDate));
         Assert.assertTrue(survey.getUploadDate().equals(uploadDate));
-        Assert.assertTrue(survey.getValues().equals(values));
+        Assert.assertTrue(survey.getValues().equals(new ArrayList<>(values.values())));
         Assert.assertTrue(survey.getScore().equals(score));
     }
 
@@ -65,7 +84,13 @@ public class SurveyShould {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Survey uid is required");
 
-        Survey.createEmptySurvey(null, "PROGRAM_UID", "ORG_UNIT_UID", "USER_UID");
+        HashMap<String, Question> questions = new HashMap<>();
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
+
+        Survey.createEmptySurvey(null, "PROGRAM_UID", "ORG_UNIT_UID", "USER_UID", questions);
     }
 
     @Test
@@ -73,7 +98,13 @@ public class SurveyShould {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Survey programUId is required");
 
-        Survey.createEmptySurvey("UID", null, "ORG_UNIT_UID", "USER_UID");
+        HashMap<String, Question> questions = new HashMap<>();
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
+
+        Survey.createEmptySurvey("UID", null, "ORG_UNIT_UID", "USER_UID", questions);
     }
 
     @Test
@@ -81,7 +112,12 @@ public class SurveyShould {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Survey orgUnitUId is required");
 
-        Survey.createEmptySurvey("UID", "PROGRAM_UID", null, "USER_UID");
+        HashMap<String, Question> questions = new HashMap<>();
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
+        Survey.createEmptySurvey("UID", "PROGRAM_UID", null, "USER_UID", questions);
     }
 
     @Test
@@ -89,6 +125,12 @@ public class SurveyShould {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Survey userUId is required");
 
-        Survey.createEmptySurvey("UID", "PROGRAM_UID", "ORG_UNIT_UID", null);
+        HashMap<String, Question> questions = new HashMap<>();
+        Question question = new Question("QuestionUID1", 2, true);
+        questions.put(question.getUId(), question);
+        question = new Question("QuestionUID2", 2, true);
+        questions.put(question.getUId(), question);
+
+        Survey.createEmptySurvey("UID", "PROGRAM_UID", "ORG_UNIT_UID", null, questions);
     }
 }
