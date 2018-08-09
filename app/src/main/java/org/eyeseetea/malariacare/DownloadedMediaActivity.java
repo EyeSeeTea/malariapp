59546ef7ff989d0f9479edc242ff468751fc19de
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-08-09 18:26:21 +0200

    Opening image files too.

--- app/src/main/java/org/eyeseetea/malariacare/DownloadedMediaActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/DownloadedMediaActivity.java
@@ -1,7 +1,9 @@
 package org.eyeseetea.malariacare;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
@@ -13,6 +15,7 @@ import org.eyeseetea.malariacare.data.database.utils.PreferencesState;
 import org.eyeseetea.malariacare.layout.adapters.downloaded_media.DownloadedMediaAdapter;
 import org.eyeseetea.malariacare.layout.utils.LayoutUtils;
 
+import java.io.File;
 import java.util.List;
 
 public class DownloadedMediaActivity extends BaseActivity {
@@ -37,15 +40,41 @@ public class DownloadedMediaActivity extends BaseActivity {
         downloadedMediaAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                openVideoActivity(mediaList.get(position).getFilename());
+                openMediaFile(mediaList.get(position));
             }
         });
     }
 
-    private void openVideoActivity(String filename) {
-        Intent videoIntent = new Intent(DashboardActivity.dashboardActivity, VideoActivity.class);
-        videoIntent.putExtra(VideoActivity.VIDEO_PATH_PARAM, filename);
+    private void openMediaFile(MediaDB media) {
+        if (media.isVideo()) {
+            openVideo(media);
+        } else if (media.isPicture()) {
+            openImage(media);
+        }
+    }
+
+    private void openVideo(MediaDB media) {
+        Intent videoIntent = new Intent(DashboardActivity.dashboardActivity,
+                VideoActivity.class);
+        videoIntent.putExtra(VideoActivity.VIDEO_PATH_PARAM, media.getFilename());
         DashboardActivity.dashboardActivity.startActivity(videoIntent);
+    }
+
+    private void openImage(MediaDB media) {
+        Intent implicitIntent = new Intent();
+        implicitIntent.setAction(Intent.ACTION_VIEW);
+        File file = new File(media.getFilename());
+        Uri contentUri = FileProvider.getUriForFile(this,
+                BuildConfig.APPLICATION_ID + ".layout.adapters.survey.FeedbackAdapter", file);
+
+        implicitIntent.setDataAndType(contentUri,
+                PreferencesState.getInstance().getContext().getContentResolver().getType(
+                        contentUri));
+        implicitIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        implicitIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+        DashboardActivity.dashboardActivity.startActivity(Intent.createChooser(implicitIntent,
+                PreferencesState.getInstance().getContext().getString(
+                        R.string.feedback_view_image)));
     }
 
     private void setActivityActionBar() {
