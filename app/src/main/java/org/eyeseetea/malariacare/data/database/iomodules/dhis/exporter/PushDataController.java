2b5e7e7f3505af88762b8ce60631eb1ae37d135f
Jorge Sánchez <xurxosf@gmail.com>
2018-10-09 07:23:39 +0200

    Fix bug to mark observation as sent

--- app/src/main/java/org/eyeseetea/malariacare/data/database/iomodules/dhis/exporter/PushDataController.java
+++ app/src/main/java/org/eyeseetea/malariacare/data/database/iomodules/dhis/exporter/PushDataController.java
@@ -24,7 +24,6 @@ import android.util.Log;
 
 import org.eyeseetea.malariacare.data.boundaries.IDataLocalDataSource;
 import org.eyeseetea.malariacare.data.boundaries.IDataRemoteDataSource;
-import org.eyeseetea.malariacare.data.database.model.ObservationDB;
 import org.eyeseetea.malariacare.data.database.utils.PreferencesState;
 import org.eyeseetea.malariacare.domain.boundary.IConnectivityManager;
 import org.eyeseetea.malariacare.domain.boundary.IPushController;
@@ -179,7 +178,7 @@ public class PushDataController implements IPushController {
                                 }
                             }
                         }else {
-                            if (!pushReport.hasPushErrors(data instanceof ObservationDB)) {
+                            if (!pushReport.hasPushErrors(data instanceof Observation)) {
                                 Log.d(TAG, "saveSurveyStatus: report without errors and status ok "
                                         + data.getSurveyUid());
                                 data.markAsSent();
