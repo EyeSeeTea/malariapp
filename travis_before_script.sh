2d77021a4f58e2369689e373e35e4e726caf47fe
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-11-09 17:25:52 +0100

    Updating version in travis.

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -3,7 +3,7 @@ echo "start travis before script"
 openssl aes-256-cbc -K $encrypted_77edd05762cb_key -iv $encrypted_77edd05762cb_iv -in app.tar.enc -out app.tar -d
 echo "decrypted file"
 tar -xvzf app.tar
-echo no | android create avd --force -n test -t android-28 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-26 --abi armeabi-v7a
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb devices
