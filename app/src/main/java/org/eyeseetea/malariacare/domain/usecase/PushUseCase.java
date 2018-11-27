68e3d73d5a4ae1e5b512c2ef404e14876181835f
Idelcano <ignaciodelcano@gmail.com>
2018-11-27 12:31:50 +0100

    remove survey checker and use case

--- app/src/main/java/org/eyeseetea/malariacare/domain/usecase/PushUseCase.java
+++ app/src/main/java/org/eyeseetea/malariacare/domain/usecase/PushUseCase.java
@@ -19,37 +19,48 @@
 
 package org.eyeseetea.malariacare.domain.usecase;
 
-import android.content.Context;
+import android.util.Log;
 
+import org.eyeseetea.malariacare.domain.boundary.IConnectivityManager;
 import org.eyeseetea.malariacare.domain.boundary.IPushController;
 import org.eyeseetea.malariacare.domain.boundary.executors.IAsyncExecutor;
 import org.eyeseetea.malariacare.domain.boundary.executors.IMainExecutor;
-import org.eyeseetea.malariacare.domain.entity.Credentials;
+import org.eyeseetea.malariacare.domain.boundary.repositories.IOrgUnitRepository;
+import org.eyeseetea.malariacare.domain.boundary.repositories.ISurveyRepository;
+import org.eyeseetea.malariacare.domain.entity.Survey;
+import org.eyeseetea.malariacare.domain.entity.SurveyStatus;
 import org.eyeseetea.malariacare.domain.exception.ConversionException;
-import org.eyeseetea.malariacare.domain.exception.NetworkException;
 import org.eyeseetea.malariacare.domain.exception.DataToPushNotFoundException;
+import org.eyeseetea.malariacare.domain.exception.NetworkException;
 import org.eyeseetea.malariacare.domain.exception.push.PushReportException;
-import org.eyeseetea.malariacare.data.remote.SurveyChecker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PushUseCase implements UseCase{
+    private final String TAG = "PushUseCase";
 
     private final IAsyncExecutor mAsyncExecutor;
     private final IMainExecutor mMainExecutor;
     private IPushController mPushController;
+    private final IConnectivityManager mConnectivityManager;
+    private final IOrgUnitRepository mOrgUnitRepository;
+    private final ISurveyRepository mSurveyRepository;
     private Callback mCallback;
-    private Credentials credentials;
-    private Context context;
 
     public PushUseCase(IAsyncExecutor asyncExecutor,
             IMainExecutor mainExecutor,
             IPushController pushController,
-                       Credentials credentials,
-                       Context context) {
+            IOrgUnitRepository orgUnitRepository,
+            ISurveyRepository surveyRepository,
+            IConnectivityManager connectivityManager
+                       ) {
         mAsyncExecutor = asyncExecutor;
         mMainExecutor = mainExecutor;
         mPushController = pushController;
-        this.credentials = credentials;
-        this.context = context;
+        mOrgUnitRepository = orgUnitRepository;
+        mSurveyRepository = surveyRepository;
+        mConnectivityManager = connectivityManager;
     }
 
     public void execute(final Callback callback) {
@@ -60,36 +71,78 @@ public class PushUseCase implements UseCase{
 
     @Override
     public void run() {
-        if (mPushController.isPushInProgress()) {
-            notifyOnPushInProgressError();
-            return;
+        boolean isNetworkAvailable = mConnectivityManager.isDeviceOnline();
+
+        if (isNetworkAvailable) {
+            if (mPushController.isPushInProgress()) {
+                notifyOnPushInProgressError();
+                return;
+            }
+            mPushController.changePushInProgress(true);
+
+
+            mMainExecutor.run(new Runnable() {
+                @Override
+                public void run() {
+                    launchQuarantineChecker();
+                }
+            });
+
+            mPushController.push(new IPushController.IPushControllerCallback() {
+                @Override
+                public void onComplete() {
+                    System.out.println("PusUseCase Complete");
+
+                    mPushController.changePushInProgress(false);
+
+                    notifyOnComplete();
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    System.out.println("PusUseCase error");
+
+                    notifyOnError(throwable);
+                }
+
+                @Override
+                public void onInformativeError(Throwable throwable) {
+                    notifyOnInformativeError(throwable);
+                }
+            });
         }
-        mPushController.changePushInProgress(true);
+    }
 
-        SurveyChecker.launchQuarantineChecker(context, credentials);
-
-        mPushController.push(new IPushController.IPushControllerCallback() {
-            @Override
-            public void onComplete() {
-                System.out.println("PusUseCase Complete");
-
-                mPushController.changePushInProgress(false);
-
-                notifyOnComplete();
+    private void launchQuarantineChecker() {
+        try {
+            List<Survey> surveys = mSurveyRepository.getSurveys(SurveyFilter.getQuarantineSurveys());
+        if(surveys!=null && surveys.size()>0) {
+            List<String> uids = new ArrayList<>();
+            for(Survey survey:surveys){
+                uids.add(survey.getUId());
             }
+            SurveyFilter surveyFilter = SurveyFilter.getSurveysUidsOnServer(uids);
+            List<Survey> quarantineSurveysInServer = mSurveyRepository.getSurveys(surveyFilter);
 
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("PusUseCase error");
+            updateSurveyStatus(surveys, quarantineSurveysInServer);
+            mSurveyRepository.save(surveys);
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-                notifyOnError(throwable);
+    private void updateSurveyStatus(List<Survey> surveys, List<Survey> quarantineSurveysInServer) {
+        for(Survey localSurvey : surveys) {
+            localSurvey.changeStatus(SurveyStatus.COMPLETED);
+            Log.d(TAG, "searching quarantine survey on server, uid: "+localSurvey.getUId());
+            for(Survey serverSurvey : quarantineSurveysInServer){
+                if(localSurvey.getUId().equals(serverSurvey.getUId())){
+                    Log.d(TAG, "quarantine survey is on server, uid: "+localSurvey.getUId());
+                    localSurvey.changeStatus(SurveyStatus.SENT);
+                }
             }
-
-            @Override
-            public void onInformativeError(Throwable throwable) {
-                notifyOnInformativeError(throwable);
-            }
-        });
+        }
     }
 
 
