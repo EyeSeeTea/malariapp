eb07a7ba20778d47634eae1713f1d8af56c8aff5
Jorge Sánchez <xurxosf@gmail.com>
2019-04-12 12:27:36 +0200

    Refactor: add recycler view to render surveys in dialog

--- app/src/main/java/org/eyeseetea/malariacare/views/MonitorSurveysDialogFragment.java
+++ app/src/main/java/org/eyeseetea/malariacare/views/MonitorSurveysDialogFragment.java
@@ -4,6 +4,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import org.eyeseetea.malariacare.data.database.model.SurveyDB;
 import org.eyeseetea.malariacare.data.database.utils.PreferencesState;
 import org.eyeseetea.malariacare.domain.entity.CompetencyScoreClassification;
 import org.eyeseetea.malariacare.domain.entity.ScoreType;
+import org.eyeseetea.malariacare.layout.adapters.monitor.SurveysMonitorAdapter;
 import org.eyeseetea.malariacare.presentation.executors.UIThreadExecutor;
 import org.eyeseetea.malariacare.utils.CompetencyUtils;
 import org.eyeseetea.malariacare.utils.DateParser;
@@ -32,6 +34,7 @@ public class MonitorSurveysDialogFragment extends DialogFragment {
     private List<SurveyDB> surveys = new ArrayList<>();
 
     private LayoutInflater inflater;
+    private View rootView;
 
     public static MonitorSurveysDialogFragment newInstance(String surveyIds) {
         MonitorSurveysDialogFragment fragment = new MonitorSurveysDialogFragment();
@@ -45,7 +48,8 @@ public class MonitorSurveysDialogFragment extends DialogFragment {
 
     @Override
     public void onStart() {
-        getDialog().getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
+        getDialog().getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
 
         super.onStart();
     }
@@ -57,66 +61,52 @@ public class MonitorSurveysDialogFragment extends DialogFragment {
 
         String surveyIds = getArguments().getString(SURVEY_IDS);
 
-        if (surveyIds != null && !surveyIds.isEmpty()){
-                String surveyIdsArray[] = surveyIds.split(";");
-                for (String uid : surveyIdsArray) {
-                    surveys.add(SurveyDB.findById(Long.parseLong(uid)));
-                }
+        if (surveyIds != null && !surveyIds.isEmpty()) {
+            String surveyIdsArray[] = surveyIds.split(";");
+            for (String uid : surveyIdsArray) {
+                surveys.add(SurveyDB.findById(Long.parseLong(uid)));
+            }
         }
 
         this.inflater = inflater;
 
-        return inflater.inflate(R.layout.dialog_survey_list_monitoring, container);
+        rootView =  inflater.inflate(R.layout.dialog_survey_list_monitoring, container);
+
+        return rootView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextView orgUnit = view.findViewById(R.id.org_unitName);
-        TextView program = view.findViewById(R.id.programName);
+        initializeOrgUnitAndProgramViews();
+        initializeCancelButton();
+        initializeRecyclerView();
+    }
+
+    private void initializeRecyclerView() {
+        RecyclerView recyclerView = rootView.findViewById(R.id.monitor_surveys_list);
+        SurveysMonitorAdapter adapter = new SurveysMonitorAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnSurveyClickListener(survey -> {
+            dismiss();
+            new UIThreadExecutor().run(
+                    () -> DashboardActivity.dashboardActivity.openFeedback(survey, false));
+        });
+
+        adapter.setSurveys(surveys);
+    }
+
+    private void initializeOrgUnitAndProgramViews() {
+        TextView orgUnit = rootView.findViewById(R.id.org_unitName);
+        TextView program = rootView.findViewById(R.id.programName);
         program.setText(surveys.get(0).getProgram().getName());
         orgUnit.setText(surveys.get(0).getOrgUnit().getName());
-        Button cancel = view.findViewById(R.id.cancel);
-        LinearLayout linearLayout = view.findViewById(R.id.dialog_content);
+    }
 
-        View row;
-
-        for(final SurveyDB survey: surveys){
-            row = inflater.inflate(R.layout.item_survey_monitoring, null);
-            TextView completionDateView = row.findViewById(R.id.survey_completion_date_item_view);
-            TextView competencyView = row.findViewById(R.id.survey_competency_item_view);
-            TextView scoreView = row.findViewById(R.id.survey_score_item_view);
-            DateParser dateParser = new DateParser();
-            completionDateView.setText(dateParser.getEuropeanFormattedDate(survey.getCompletionDate()));
-            scoreView.setText(Math.round(survey.getMainScore())+"");
-            Resources resources = PreferencesState.getInstance().getContext().getResources();
-
-            ScoreType scoreType = new ScoreType(survey.getMainScore());
-            if (scoreType.isTypeA()) {
-                scoreView.setBackgroundColor(resources.getColor(R.color.high_score_color));
-            }else if (scoreType.isTypeB()){
-                scoreView.setBackgroundColor(resources.getColor(R.color.medium_score_color));
-            }else if (scoreType.isTypeC()){
-                scoreView.setBackgroundColor(resources.getColor(R.color.low_score_color));
-            }
-
-            CompetencyScoreClassification classification =
-                    CompetencyScoreClassification.get(
-                            survey.getCompetencyScoreClassification());
-
-            CompetencyUtils.setBackgroundByCompetency(competencyView, classification);
-            CompetencyUtils.setTextByCompetency(competencyView, classification);
-            CompetencyUtils.setTextColorByCompetency(competencyView, classification);
-
-            row.setOnClickListener(view1 -> {
-                dismiss();
-                new UIThreadExecutor().run(
-                        () -> DashboardActivity.dashboardActivity.openFeedback(survey, false));
-            });
-            linearLayout.addView(row );
-        }
-
+    private void initializeCancelButton() {
+        Button cancel = rootView.findViewById(R.id.cancel);
         cancel.setOnClickListener(v -> dismiss());
     }
 }
