4f4992b64d50303cecc132ec4c0f6cde88d19d28
Jorge Sánchez <xurxosf@gmail.com>
2019-05-07 10:26:19 +0200

    Remove unnecessary nextScheduleMonths

--- app/src/test/java/org/eyeseetea/malariacare/domain/entity/ServerShould.java
+++ app/src/test/java/org/eyeseetea/malariacare/domain/entity/ServerShould.java
@@ -11,22 +11,16 @@ import static org.junit.Assert.assertThat;
 
 public class ServerShould {
 
-    public static final String DEFAULT_SCHEDULE_MONTHS_VALUE ="https://data.psi-mis.org";
-    public static final HashMap<String, int[]> nextScheduleMonths = new HashMap<>();
-
-    static {
-        nextScheduleMonths.put(DEFAULT_SCHEDULE_MONTHS_VALUE, new int[]{2, 4, 6});
-        nextScheduleMonths.put("https://zw.hnqis.org/", new int[]{1, 1, 6});
-    }
+    public static final String DEFAULT_URL ="https://data.psi-mis.org";
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void return_values_when_is_created_with_correct_values() {
-        Server server = new Server(DEFAULT_SCHEDULE_MONTHS_VALUE);
+        Server server = new Server(DEFAULT_URL);
 
-        assertThat(server.getUrl(), is(DEFAULT_SCHEDULE_MONTHS_VALUE));
+        assertThat(server.getUrl(), is(DEFAULT_URL));
     }
 
 
