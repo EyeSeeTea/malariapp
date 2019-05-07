a1f5652c7b3d55cfbe14bf579225ccbe8931b67b
Jorge Sánchez <xurxosf@gmail.com>
2019-05-07 10:13:54 +0200

    Fix Server Tests

--- app/src/test/java/org/eyeseetea/malariacare/domain/entity/ServerShould.java
+++ app/src/test/java/org/eyeseetea/malariacare/domain/entity/ServerShould.java
@@ -24,42 +24,16 @@ public class ServerShould {
 
     @Test
     public void return_values_when_is_created_with_correct_values() {
-        Server server = new Server(DEFAULT_SCHEDULE_MONTHS_VALUE, new NextScheduleMonths(nextScheduleMonths.get(DEFAULT_SCHEDULE_MONTHS_VALUE)));
+        Server server = new Server(DEFAULT_SCHEDULE_MONTHS_VALUE);
 
         assertThat(server.getUrl(), is(DEFAULT_SCHEDULE_MONTHS_VALUE));
-        assertThat(server.getNextScheduleMatrix().getScoreAMonths(), is(6));
-        assertThat(server.getNextScheduleMatrix().getHighProductivityMonths(), is(2));
-        assertThat(server.getNextScheduleMatrix().getLowProductivityMonths(), is(4));
     }
 
-    @Test
-    public void return_correct_values_when_is_created_with_zimbabwe_values() {
-        Server server = new Server(DEFAULT_SCHEDULE_MONTHS_VALUE, new NextScheduleMonths(nextScheduleMonths.get("https://zw.hnqis.org/")));
-
-        assertThat(server.getUrl(), is(DEFAULT_SCHEDULE_MONTHS_VALUE));
-        assertThat(server.getNextScheduleMatrix().getScoreAMonths(), is(6));
-        assertThat(server.getNextScheduleMatrix().getLowProductivityMonths(), is(1));
-        assertThat(server.getNextScheduleMatrix().getHighProductivityMonths(), is(1));
-    }
 
     @Test
     public void throw_exception_when_url_is_null() {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("url is required");
-        new Server(null, new NextScheduleMonths(nextScheduleMonths.get("https://zw.hnqis.org/")));
-    }
-
-    @Test
-    public void throw_exception_when_next_schedule_months_is_null() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("nextScheduleMatrix is required");
-        new Server(DEFAULT_SCHEDULE_MONTHS_VALUE, null);
-    }
-
-    @Test
-    public void throw_exception_when_next_schedule_months_has_invalid_matrix() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("array with three dimensions required");
-        new Server(DEFAULT_SCHEDULE_MONTHS_VALUE, new NextScheduleMonths(new int[]{1, 1}));
+        new Server(null);
     }
 }
