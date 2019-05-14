5cc9feb85347ea6b33163d4cdb9973c38c5f6ce4
Jorge Sánchez <xurxosf@gmail.com>
2019-05-14 08:26:51 +0200

    Fix bugs

--- app/src/main/java/org/eyeseetea/malariacare/presentation/viewmodels/observations/QuestionViewModel.java
+++
@@ -1,9 +0,0 @@
-package org.eyeseetea.malariacare.presentation.viewmodels.observations;
-
-public class QuestionViewModel extends MissedStepViewModel {
-    public QuestionViewModel(long questionId, long compositeScoreParentId, String name) {
-        super(QUESTION_KEY_PREFIX + questionId,
-                COMPOSITE_KEY_PREFIX + compositeScoreParentId,
-                name, false);
-    }
-}
