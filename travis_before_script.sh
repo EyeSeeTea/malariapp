a1c5a87e2c56623e54abc809c0f009d925d23b20
Jorge Sánchez <xurxosf@gmail.com>
2019-12-17 08:26:48 +0100

    Update travis encrypted files

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start travis before script"
-openssl aes-256-cbc -K $encrypted_77edd05762cb_key -iv $encrypted_77edd05762cb_iv -in app.tar.enc -out app.tar -d
+openssl aes-256-cbc -K $encrypted_b4da38d765e0_key -iv $encrypted_b4da38d765e0_iv -in app.tar.enc -out app.tar -d
 echo "decrypted file"
 tar -xvzf app.tar
 echo "continue"
