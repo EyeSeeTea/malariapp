9b363513b79b854c196f03ec0a8ea5e539312d32
Jorge Sánchez <xurxosf@gmail.com>
2019-12-27 12:15:52 +0100

    Convert ServerLocalDataSource to Kotlin

---
+++ app/src/main/java/org/eyeseetea/malariacare/data/database/datasources/ServerLocalDataSource.kt
@@ -0,0 +1,61 @@
+package org.eyeseetea.malariacare.data.database.datasources
+
+import android.util.Log
+import com.raizlabs.android.dbflow.data.Blob
+import com.raizlabs.android.dbflow.sql.language.Delete
+import org.eyeseetea.malariacare.data.ReadableServerDataSource
+import org.eyeseetea.malariacare.data.WritableServerDataSource
+import org.eyeseetea.malariacare.data.database.model.ServerDB
+import org.eyeseetea.malariacare.domain.entity.Server
+
+class ServerLocalDataSource : ReadableServerDataSource, WritableServerDataSource {
+    override fun get(): Server {
+        Log.d(this.javaClass.simpleName, "Retrieving connected server from local Database")
+        val serverDB = ServerDB.getConnectedServerFromDB()
+
+        return if (serverDB == null) {
+            // TODO: server either
+            Server("NotFound")
+        } else {
+            mapServer(serverDB)
+        }
+    }
+
+    override fun saveAll(servers: List<Server>) {
+        Delete.table(ServerDB::class.java)
+        for (server in servers) {
+            save(server)
+        }
+    }
+
+    override fun save(server: Server) {
+        var serverDB = ServerDB.getServerFromDByUrl(server.url)
+
+        if (serverDB == null) {
+            serverDB = ServerDB()
+        }
+        serverDB.url = server.url
+        serverDB.name = server.name
+        serverDB.isConnected = server.isConnected
+
+        if (server.logo != null) {
+            serverDB.logo = Blob(server.logo)
+        }
+        serverDB.save()
+    }
+
+    override fun getAll(): List<Server> {
+        Log.d(this.javaClass.simpleName, "Retrieving Servers from local Database")
+        return ServerDB.getAllServersFromDB().map { serverDB -> mapServer(serverDB) }
+    }
+
+
+    private fun mapServer(serverDB: ServerDB): Server {
+        return Server(
+            serverDB.url,
+            serverDB.name,
+            serverDB.logo?.blob,
+            serverDB.isConnected
+        )
+    }
+}
