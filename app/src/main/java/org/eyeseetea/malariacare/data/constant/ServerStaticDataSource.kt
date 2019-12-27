d50d0b3b56d1f59048cf9384fb10c96d65500aa1
Jorge Sánchez <xurxosf@gmail.com>
2019-12-27 11:33:44 +0100

    Convert ServerStaticDataSource to Kotlin

---
+++ app/src/main/java/org/eyeseetea/malariacare/data/constant/ServerStaticDataSource.kt
@@ -0,0 +1,21 @@
+package org.eyeseetea.malariacare.data.constant
+
+import android.content.Context
+import android.util.Log
+import org.eyeseetea.malariacare.R
+import org.eyeseetea.malariacare.data.ReadableServerDataSource
+import org.eyeseetea.malariacare.domain.entity.Server
+
+class ServerStaticDataSource(private val context: Context) :
+    ReadableServerDataSource {
+    override fun getAll(): List<Server> {
+        Log.d(this.javaClass.simpleName, "Retrieving Servers from static array string")
+        val serverUrls = context.resources.getStringArray(R.array.server_list)
+
+        return serverUrls.map { url -> Server(url) }
+    }
+
+    override fun get(): Server { // not implemented
+        return Server("Not found")
+    }
+}
